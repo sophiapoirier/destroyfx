75a3c7be53a4d035d5a70a0916074b5e93b61f06
Dr. Tom Murphy VII Ph.D <499244+tom7@users.noreply.github.com>
2001-08-11 18:42:50 +0000

    initial import

---
+++ stub-plugin/stub.cpp
@@ -0,0 +1,143 @@
+
+#include "stub.hpp"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <math.h>
+
+PLUGIN::PLUGIN(audioMasterCallback audioMaster)
+  : AudioEffectX(audioMaster, 1, NUM_PARAMS) {
+
+  /* FPARAM(samplehold, 0, "sample hold", 0.2, "msec"); */
+
+  setup();
+}
+
+PLUGIN::~PLUGIN() {
+
+}
+
+void PLUGIN::setParameter(long index, float value) {
+  switch (index) {
+
+    /*
+  case 1:
+    samplehold = value;
+    break;
+    */
+  default:
+    break;
+  }
+}
+
+float PLUGIN::getParameter(long index) {
+  switch (index) {
+    /* special cases here */
+  default:
+    /* otherwise pull it out of array. */
+    if (index >= 0 && index < NUM_PARAMS) return *paramptrs[index].ptr;
+    else return 0.0; /* ? */
+  }
+}
+
+void PLUGIN::getParameterName(long index, char *label) {
+  switch(index) {
+    /* special cases here */
+  default:
+    if (index >= 0 && index < NUM_PARAMS && paramptrs[index].name) 
+      strcpy(label, paramptrs[index].name);
+    else strcpy(label, "?");
+    break;
+  }
+}
+
+void PLUGIN::getParameterDisplay(long index, char *text) {
+  switch(index) {
+    /* special cases here */
+  default:
+    float2string(getParameter(index), text);
+    break;
+  }
+}
+
+void PLUGIN::getParameterLabel(long index, char *label) {
+  switch(index) {
+    /* special cases here */
+  default:
+    if (index >= 0 && index < NUM_PARAMS && paramptrs[index].units) 
+      strcpy(label, paramptrs[index].units);
+    else strcpy(label, "?");
+    break;
+  }
+}
+
+void PLUGIN::suspend () {
+
+}
+
+void PLUGIN::process(float **inputs, float **outputs, long samples) {
+  float * in  = *inputs;
+  float * out = *outputs;
+
+  /* ... */
+}
+
+/* usually this will do */
+void PLUGIN::processReplacing(float **inputs, float **outputs, long samples) {
+  for (int i = 0; i < samples; i++) {
+    outputs[0][i] = 0.0;
+  }
+  process(inputs,outputs,samples);
+}
+
+
+
+/* XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX */ 
+/* ---------- boring stuff below this line ----------- */
+/* XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX */ 
+
+static AudioEffect *effect = 0;
+bool oome = false;
+
+#if MAC
+   #pragma export on
+#endif
+
+// prototype of the export function main
+#if BEOS
+   #define main main_plugin
+   extern "C" __declspec(dllexport) AEffect 
+      *main_plugin (audioMasterCallback audioMaster);
+
+#else
+   AEffect *main (audioMasterCallback audioMaster);
+#endif
+
+AEffect *main (audioMasterCallback audioMaster) {
+  // get vst version
+  if (!audioMaster (0, audioMasterVersion, 0, 0, 0, 0))
+    return 0;  // old version
+
+  effect = new PLUGIN (audioMaster);
+  if (!effect)
+    return 0;
+  if (oome) {
+    delete effect;
+    return 0;
+  }
+  return effect->getAeffect ();
+}
+
+#if MAC
+  #pragma export off
+#endif
+
+#if WIN32
+  void* hInstance;
+  BOOL WINAPI DllMain (HINSTANCE hInst, DWORD dwReason, LPVOID lpvReserved) {
+    hInstance = hInst;
+    return 1;
+  }
+#endif
