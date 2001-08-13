3c382e2da05bf376df255964a5e95805b39dff48
Dr. Tom Murphy VII Ph.D <499244+tom7@users.noreply.github.com>
2001-08-13 03:02:10 +0000

    added decimate plugin

---
+++ decimate/decimate.h
@@ -0,0 +1,33 @@
+
+#ifndef __DECIMATE_H
+#define __DECIMATE_H
+
+#include "audioeffectx.h"
+
+class Decimate : public AudioEffectX {
+public:
+  Decimate(audioMasterCallback audioMaster);
+  ~Decimate();
+
+  virtual void process(float **inputs, float **outputs, long sampleFrames);
+  virtual void processReplacing(float **inputs, 
+                                float **outputs, long sampleFrames);
+  virtual void setProgramName(char *name);
+  virtual void getProgramName(char *name);
+  virtual void setParameter(long index, float value);
+  virtual float getParameter(long index);
+  virtual void getParameterLabel(long index, char *label);
+  virtual void getParameterDisplay(long index, char *text);
+  virtual void getParameterName(long index, char *text);
+
+protected:
+  float bitres;
+  float destruct;
+  char programName[32];
+
+  float bit1, bit2;
+  int samplesleft;
+  float samplehold;
+};
+
+#endif
