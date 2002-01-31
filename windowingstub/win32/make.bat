e3703fb1340f9b1a93e6a3f9b4ee34c789fe146b
Dr. Tom Murphy VII Ph.D <499244+tom7@users.noreply.github.com>
2002-01-31 06:55:42 +0000

    makefile

---
+++ windowingstub/win32/make.bat
@@ -0,0 +1,2 @@
+
+cl /nologo /O2 /Ot /Og /Oi /Oy /Gs /GD /I..\vstsdk\ /LD ..\windowingstub\windowingstub.cpp ..\vstsdk\AudioEffect.cpp ..\vstsdk\audioeffectx.cpp windowingstub.def /Fec:\progra~1\steinberg\vstplugins\dfx-windowingstub.dll
