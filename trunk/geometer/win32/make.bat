a2b856b9bb528f2c8e6b2e0f3359a82b738bf235
tom7 <tom7@b413c13a-479c-4ba3-9490-20a46969969a>
2002-02-06 04:39:26 +0000

    added geometer again

---
+++ trunk/geometer/win32/make.bat
@@ -0,0 +1,2 @@
+
+cl /nologo /O2 /Ot /Og /Oi /Oy /Gs /GD /I..\vstsdk\ /LD ..\geometer\geometer.cpp ..\vstsdk\AudioEffect.cpp ..\vstsdk\audioeffectx.cpp geometer.def /Fec:\progra~1\steinberg\vstplugins\dfx-geometer.dll
