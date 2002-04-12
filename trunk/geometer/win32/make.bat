77ac26211de53006683f375b41123533c5a54429
tom7 <tom7@b413c13a-479c-4ba3-9490-20a46969969a>
2002-04-12 02:44:32 +0000

    build for gui

--- trunk/geometer/win32/make.bat
+++ trunk/geometer/win32/make.bat
@@ -1,2 +1,4 @@
 
-cl /nologo /O2 /Ot /Og /Oi /Oy /Gs /GD /I..\vstsdk\ /LD ..\geometer\geometer.cpp ..\vstsdk\AudioEffect.cpp ..\vstsdk\audioeffectx.cpp geometer.def /Fec:\progra~1\steinberg\vstplugins\dfx-geometer.dll
+rc geometer.rc
+
+cl /nologo /O2 /Ot /Og /Oi /Oy /Gs /GD /I..\vstsdk\ /I..\dfx-library\ /LD /D "GEOMETER_EXPORTS" ..\geometer\geometer.cpp ..\vstsdk\AudioEffect.cpp ..\vstsdk\audioeffectx.cpp geometer.def geometer.res ..\vstsdk_win32\vstgui.lib /Fec:\progra~1\steinberg\vstplugins\dfx-geometer.dll
