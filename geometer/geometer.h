7821e9287863c0708df1286f79fb4e0da8890f58
Dr. Tom Murphy VII Ph.D <499244+tom7@users.noreply.github.com>
2002-04-12 01:59:00 +0000

    last non-gui build.

--- geometer/geometer.h
+++ geometer/geometer.h
@@ -1,15 +1,21 @@
 
-/* Super Destroy FX Windowing System! */
+/* Geometer,
+   featuring the Super Destroy FX Windowing System! */
 
-#ifndef __DFX_WINDOWINGSTUB_H
-#define __DFX_WINDOWINGSTUB_H
+#ifndef __DFX_GEOMETER_H
+#define __DFX_GEOMETER_H
 
 #include <audioeffectx.h>
 
+#ifdef GUI
+  #include "dfxmisc.h"
+  #include "VstChunk.h"
+#endif
+
 #ifdef WIN32
 /* turn off warnings about default but no cases in switch, etc. */
-   #pragma warning( disable : 4065 57 4200 4244 )
-   #include <windows.h>
+    #pragma warning( disable : 4065 57 4200 4244 )
+    #include <windows.h>
 #endif
 
 #define fsign(f) ((f<0.0)?-1.0:1.0)
@@ -18,14 +24,16 @@
 /* change these for your plugins */
 #define PLUGIN Geometer
 #define PLUGINID 'DFgr'
-#define PLUGINNAME "DFX WINDOWINGSTUB"
+#define PLUGINNAME "DFX GEOMETER"
 #define PLUGINPROGRAM GeometerProg
 
 #define NUM_PROGRAMS 16
 
+//#define GUI
+
 /* the names of the parameters */
-enum { P_BUFSIZE, P_SHAPE, 
-       P_POINTSTYLE, 
+enum { P_BUFSIZE, P_SHAPE,
+       P_POINTSTYLE,
        P_POINTPARAM0,
        P_POINTPARAM1,
        P_POINTPARAM2,
@@ -44,22 +52,22 @@ enum { P_BUFSIZE, P_SHAPE,
        P_INTERPARAM6,
        P_INTERPARAM7,
        P_POINTOP1, P_POINTOP2, P_POINTOP3,
-       P_OPPAR1_0, 
-       P_OPPAR1_1, 
-       P_OPPAR1_2, 
-       P_OPPAR1_3, 
-       P_OPPAR1_4, 
-       P_OPPAR1_5, 
-       P_OPPAR1_6, 
-       P_OPPAR1_7, 
-       P_OPPAR2_0, 
-       P_OPPAR2_1, 
-       P_OPPAR2_2, 
-       P_OPPAR2_3, 
-       P_OPPAR2_4, 
-       P_OPPAR2_5, 
-       P_OPPAR2_6, 
-       P_OPPAR2_7, 
+       P_OPPAR1_0,
+       P_OPPAR1_1,
+       P_OPPAR1_2,
+       P_OPPAR1_3,
+       P_OPPAR1_4,
+       P_OPPAR1_5,
+       P_OPPAR1_6,
+       P_OPPAR1_7,
+       P_OPPAR2_0,
+       P_OPPAR2_1,
+       P_OPPAR2_2,
+       P_OPPAR2_3,
+       P_OPPAR2_4,
+       P_OPPAR2_5,
+       P_OPPAR2_6,
+       P_OPPAR2_7,
        P_OPPAR3_0,
        P_OPPAR3_1,
        P_OPPAR3_2,
@@ -72,153 +80,162 @@ enum { P_BUFSIZE, P_SHAPE,
 
 
 struct param {
-  float * ptr;
-  const char * name;
-  const char * units;
-  float def;
+   float * ptr;
+   const char * name;
+   const char * units;
+   float def;
 };
 
 class PLUGINPROGRAM {
-  friend class PLUGIN;
+   friend class PLUGIN;
 public:
-  PLUGINPROGRAM() { strcpy(name, "default"); }
-  ~PLUGINPROGRAM() { }
-  void init(class PLUGIN *);
+   PLUGINPROGRAM() { strcpy(name, "default"); }
+   ~PLUGINPROGRAM() { }
+   void init(class PLUGIN *);
 
 private:
 
-  char name[32];
-  float param[NUM_PARAMS];
+   char name[32];
+   float param[NUM_PARAMS];
 };
 
 class PLUGIN : public AudioEffectX {
-  friend class PLUGINPROGRAM;
+   friend class PLUGINPROGRAM;
 public:
-  PLUGIN(audioMasterCallback audioMaster);
-  ~PLUGIN();
+   PLUGIN(audioMasterCallback audioMaster);
+   ~PLUGIN();
 
-  virtual void processX(float **inputs, float **outputs, long sampleFrames,
-		       int replacing);
-  virtual void process(float **inputs, float **outputs, long sampleFrames);
-  virtual void processReplacing(float **inputs, float **outputs, 
-				long sampleFrames);
+   virtual void processX(float **inputs, float **outputs, long sampleFrames,
+                       int replacing);
+   virtual void process(float **inputs, float **outputs, long sampleFrames);
+   virtual void processReplacing(float **inputs, float **outputs,
+                                long sampleFrames);
 
-  virtual void setParameter(long index, float value);
-  virtual float getParameter(long index);
-  virtual void getParameterLabel(long index, char *label);
-  virtual void getParameterDisplay(long index, char *text);
-  virtual void getParameterName(long index, char *text);
+   virtual void setParameter(long index, float value);
+   virtual float getParameter(long index);
+   virtual void getParameterLabel(long index, char *label);
+   virtual void getParameterDisplay(long index, char *text);
+   virtual void getParameterName(long index, char *text);
 
-  virtual void setProgram(long programNum);
-  virtual void setProgramName(char *name);
-  virtual void getProgramName(char *name);
-  virtual bool getProgramNameIndexed(long category, long index, char *text);
-  virtual bool copyProgram(long destination);
+   virtual void setProgram(long programNum);
+   virtual void setProgramName(char *name);
+   virtual void getProgramName(char *name);
+   virtual bool getProgramNameIndexed(long category, long index, char *text);
+   virtual bool copyProgram(long destination);
 
-  virtual long getTailSize();
+   virtual long getTailSize();
 
-  virtual void suspend();
-  virtual void resume();
+   virtual void suspend();
+   virtual void resume();
 
-  virtual void processw(float * in, float * out, long samples);
+   virtual long processEvents(VstEvents* events);
+   virtual long getChunk(void **data, bool isPreset);
+   virtual long setChunk(void *data, long byteSize, bool isPreset);
 
-  bool getVendorString(char *text) {
-    strcpy (text, "Destroy FX");
-    return true; 
-  }
+   virtual void processw(float * in, float * out, long samples);
 
-  bool getProductString(char *text) {
-    strcpy (text, "Super Destroy FX bipolar VST plugin pack");
-    return true; 
-  }
+   bool getVendorString(char *text) {
+     strcpy (text, "Destroy FX");
+     return true;
+   }
 
-  bool getEffectName(char *name) {
-    strcpy (name, PLUGINNAME);
-    return true; 
-  }
+   bool getProductString(char *text) {
+     strcpy (text, "Super Destroy FX bipolar VST plugin pack");
+     return true;
+   }
 
-  void setup() {
-    programs = new PLUGINPROGRAM[NUM_PROGRAMS];
-    for(int i = 0; i < NUM_PROGRAMS; i++) programs[i].init(this);
-    setProgram(0);
-    strcpy(programs[0].name, "VST window");	// default program name
+   bool getEffectName(char *name) {
+     strcpy (name, PLUGINNAME);
+     return true;
+   }
 
-    setNumInputs(1);		/* mono in/out */
-    setNumOutputs(1);
-    setUniqueID(PLUGINID);
+   void setup() {
+     programs = new PLUGINPROGRAM[NUM_PROGRAMS];
+     for(int i = 0; i < NUM_PROGRAMS; i++) programs[i].init(this);
+     setProgram(0);
+     strcpy(programs[0].name, "VST window");    // default program name
 
-    canProcessReplacing();
-  }
+     setNumInputs(1);           /* mono in/out */
+     setNumOutputs(1);
+     setUniqueID(PLUGINID);
+
+     canProcessReplacing();
+   }
+
+   /* this stuff is public so that the GUI can see it */
+   VstChunk *chunk;     // chunky data full of parameter settings & stuff
 
 protected:
-  /* stores info for each parameter */
-  param paramptrs[NUM_PARAMS];
+   /* stores info for each parameter */
+   param paramptrs[NUM_PARAMS];
 
-  /* stores programs */
-  PLUGINPROGRAM *programs;
+   /* stores programs */
+   PLUGINPROGRAM *programs;
 
-  /* size of buffer (param) */
-  float bufsizep;
-  /* shape of envelope */
-  float shape;
+   /* size of buffer (param) */
+   float bufsizep;
+   /* shape of envelope */
+   float shape;
 
-  /* input and output buffers. out is framesize*2 samples long, in is framesize
-     samples long. (for maximum framesize)
-  */
-  float * in0, * out0;
-
-  /* third-sized tail of previous processed frame. already has mixing envelope
-     applied.
+   /* input and output buffers. out is framesize*2 samples long, in is
+      framesize samples long. (for maximum framesize)
    */
-  float * prevmix;
+   float * in0, * out0;
 
-  /* number of samples in in0 */
-  int insize;
+   /* third-sized tail of previous processed frame. already has mixing envelope
+      applied.
+    */
+   float * prevmix;
 
-  /* number of samples and starting position of valid samples in out0 */
-  int outsize;
-  int outstart;
+   /* number of samples in in0 */
+   int insize;
 
-  #define BUFFERSIZESSIZE 15
-  static const int buffersizes[BUFFERSIZESSIZE];
+   /* number of samples and starting position of valid samples in out0 */
+   int outsize;
+   int outstart;
 
-  /* buffersize is 3 * third, framesize is 2 * third 
-     buffersize is used for outbuf.
-  */
-  long bufsize, framesize, third;
+   #define BUFFERSIZESSIZE 15
+   static const int buffersizes[BUFFERSIZESSIZE];
 
-  /* 1 if need to do ioChanged since buffer settings are different now */
-  int changed;
+   /* buffersize is 3 * third, framesize is 2 * third
+      buffersize is used for outbuf.
+   */
+   long bufsize, framesize, third;
 
-  /* ---------- geometer stuff ----------- */
+   /* 1 if need to do ioChanged since buffer settings are different now */
+   int changed;
 
-  int pointops(float pop, int npts, float * op_param, int samps);
+   /* ---------- geometer stuff ----------- */
 
-  float pointstyle;
-  float pointparam[8];
+   int pointops(float pop, int npts, float * op_param, int samps);
 
-  float interpstyle;
-  float interparam[8];
+   float pointstyle;
+   float pointparam[8];
 
-  float pointop1;
-  float pointop2;
-  float pointop3;
+   float interpstyle;
+   float interparam[8];
 
-  float oppar1[8];
-  float oppar2[8];
-  float oppar3[8];
+   float pointop1;
+   float pointop2;
+   float pointop3;
 
-  int lastx;
-  float lasty;
+   float oppar1[8];
+   float oppar2[8];
+   float oppar3[8];
 
-  int * pointx;
-  float * pointy;
+   int lastx;
+   float lasty;
 
-  int * tempx;
-  float * tempy;
+   int * pointx;
+   float * pointy;
+
+   int * tempx;
+   float * tempy;
 
 };
 
-#define FPARAM(pname, idx, nm, init, un) do { paramptrs[idx].ptr = &pname; paramptrs[idx].name = (nm); paramptrs[idx].units = (un); paramptrs[idx].def = (init); pname = paramptrs[idx].def; } while (0)
+#define FPARAM(pname, idx, nm, init, un) do { paramptrs[idx].ptr = 
+&pname; paramptrs[idx].name = (nm); paramptrs[idx].units = (un); 
+paramptrs[idx].def = (init); pname = paramptrs[idx].def; } while (0)
 
 #endif
