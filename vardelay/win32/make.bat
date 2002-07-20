be0a8eeeb5a4adbf26189d717b49ec00d797b6dd
Dr. Tom Murphy VII Ph.D <499244+tom7@users.noreply.github.com>
2002-07-20 21:35:48 +0000

    add vardelay

---
+++ vardelay/win32/make.bat
@@ -0,0 +1,3 @@
+
+cl /nologo /O2 /Ot /Og /Oi /Oy /Gs /GD /I..\vstsdk\ /LD ..\vardelay\vardelay.cpp ..\vstsdk\AudioEffect.cpp ..\vstsdk\audioeffectx.cpp vardelay.def "/Fec:\progra~1\steinberg\vstplugins\DFX Vardelay.dll"
+
