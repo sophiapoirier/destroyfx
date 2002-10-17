008f02901ed91b1ca77b94604222d5f4bb650b3f
marc3 <2997196+sophiapoirier@users.noreply.github.com>
2002-10-17 22:02:14 +0000

    first commit of Transverb ported to our new DfxPlugin format

--- transverb/transverb.h
+++ transverb/transverb.h
@@ -3,14 +3,11 @@
 
 /* DFX Transverb plugin by Tom 7 and Marc 3 */
 
-#include <stdio.h>
-
-#include "dfxmisc.h"
+#include "dfxplugin.h"
 #include "iirfilter.h"
-#include "vstchunk.h"
 
 
-//----------------------------------------------------------------------------- 
+//-----------------------------------------------------------------------------
 // these are the plugin parameters:
 enum
 {
@@ -29,24 +26,11 @@ enum
 	kSpeed1mode,
 	kSpeed2mode,
 
-	NUM_PARAMS
+	NUM_PARAMETERS
 };
 
 
-#define NUM_PROGRAMS 16
-
-#define fsign(f) ((f<0.0f)?-1.0f:1.0f)
-
-#define PLUGIN Transverb
-#define PLUGINID 'DFtv'
-#define PLUGIN_VERSION 1400
-#ifdef TRANSVERB_STEREO
-  #define PLUGINNAME "DFX Transverb"
-  #define NUM_CHANNELS 2
-#else
-  #define PLUGINNAME "DFX Transverb (mono)"
-  #define NUM_CHANNELS 1
-#endif
+#define NUM_PRESETS 16
 
 #define BUFFER_MIN 1.0f
 #define BUFFER_MAX 3000.0f
@@ -59,27 +43,18 @@ enum
 #define SPEED_MAX 6.0f
 #define speedScaled(A)   ( paramRangeScaled((A), SPEED_MIN, SPEED_MAX) )
 #define speedUnscaled(A)   ( paramRangeUnscaled((A), SPEED_MIN, SPEED_MAX) )
-// for backwards compatibility with versions 1.0 & 1.0.1
-#define OLD_SPEED_MIN 0.03f
-#define OLD_SPEED_MAX 10.0f
-#define oldSpeedScaled(A)   ( paramRangeSquaredScaled((A), OLD_SPEED_MIN, OLD_SPEED_MAX) )
 
 #define qualityScaled(A)   ( paramSteppedScaled((A), numQualities) )
 #define qualityUnscaled(A)   ( paramSteppedUnscaled((A), numQualities) )
 
 #define SMOOTH_DUR 42
 
-// this is for converting version 1.0 speed parameter valuess to the current format
-//#define newSpeed(A)   ((log2f(oldSpeedScaled((A)))-SPEED_MIN) / (SPEED_MAX-SPEED_MIN))
-#define newSpeed(A)   (((logf(oldSpeedScaled((A)))/logf(2.0f))-SPEED_MIN) / (SPEED_MAX-SPEED_MIN))
 
 // this stuff is for the speed parameter adjustment mode switch on the GUI
 enum { kFineMode, kSemitoneMode, kOctaveMode, numSpeedModes };
 #define speedModeScaled(A)   ( paramSteppedScaled((A), numSpeedModes) )
 
-#define numFIRtaps 23
-
-const float RAND_MAX_FLOAT = (float) RAND_MAX;	// reduces wasteful casting
+#define NUM_FIR_TAPS 23
 
 
 enum { dirtfi, hifi, ultrahifi, numQualities };
@@ -87,135 +62,68 @@ enum { dirtfi, hifi, ultrahifi, numQualities };
 enum { useNothing, useHighpass, useLowpassIIR, useLowpassFIR, numFilterModes };
 
 
-struct param {
-  float * ptr;
-  const char * name;
-  const char * units;
-};
 
-class TransverbProgram
-{
-friend struct PLUGIN;
+class TransverbDSP : public DfxPluginCore {
+
 public:
-	TransverbProgram();
-	~TransverbProgram();
-private:	
-	float *parameter;
-	char *name;
-};
+  TransverbDSP(TARGET_API_CORE_INSTANCE_TYPE *inInstance);
+  ~TransverbDSP();
 
-struct PLUGIN : public AudioEffectX {
+  virtual void process(const float *in, float *out, unsigned long inNumFrames, bool replacing=true);
+  virtual void reset();
+  virtual void processparameters();
+  virtual bool createbuffers();
+  virtual void clearbuffers();
+  virtual void releasebuffers();
 
-  PLUGIN(audioMasterCallback audioMaster);
-  ~PLUGIN();
-
-  virtual void processX(float **inputs, float **outputs, long sampleFrames,
-			int replacing);
-  virtual void process(float **inputs, float **outputs, long sampleFrames);
-  virtual void processReplacing(float **inputs, float **outputs, 
-				long sampleFrames);
-  virtual long processEvents(VstEvents* events);
-
-  virtual void setParameter(long index, float value);
-  virtual float getParameter(long index);
-  virtual void getParameterLabel(long index, char *label);
-  virtual void getParameterDisplay(long index, char *text);
-  virtual void getParameterName(long index, char *text);
-
-  virtual bool getInputProperties(long index, VstPinProperties* properties);
-  virtual bool getOutputProperties(long index, VstPinProperties* properties);
-  virtual long canDo(char* text);
-
-  virtual void suspend();
-  virtual void resume();
-
-  bool getVendorString(char *text) {
-    strcpy (text, "Destroy FX");
-    return true;
-  }
-
-  long getVendorVersion() {
-    return PLUGIN_VERSION;
-  }
-
-  bool getProductString(char *text) {
-    strcpy (text, "Super Destroy FX bipolar VST plugin pack");
-    return true;
-  }
-
-  bool getEffectName(char *name) {
-    strcpy (name, PLUGINNAME);
-    return true;
-  }
-
-  void setup() {
-    setNumInputs(NUM_CHANNELS);
-    setNumOutputs(NUM_CHANNELS);
-  #ifdef TRANSVERB_STEREO
-    canMono();
-    setUniqueID(PLUGINID);	// 'DFtv'
-  #else
-    setUniqueID('DtvM');
-  #endif
-
-    canProcessReplacing();
-  }
-
-  virtual void setProgram(long programNum);
-  virtual void setProgramName(char *name);
-  virtual void getProgramName(char *name);
-  virtual bool getProgramNameIndexed(long category, long index, char *text);
-  virtual bool copyProgram(long destination);
-
-  virtual long getChunk(void **data, bool isPreset) {
-    return chunk->getChunk(data, isPreset);
-  }
-  virtual long setChunk(void *data, long byteSize, bool isPreset) {
-    return chunk->setChunk(data, byteSize, isPreset);
-  }
-
-// the GUI needs to be able to look at these, so they are public
-  void randomizeParameters(bool writeAutomation = false);
-  float fBsize;	// parameter
-  VstChunk *chunk;
-
-protected:
-  void initPresets();
-  void createAudioBuffers();
-  void clearBuffers();
-  TransverbProgram *programs;
-
-  param paramptrs[NUM_PARAMS];
-
-  float drymix;
+private:
+  // these get set to the parameter values
   int bsize;
-  float mix1, speed1, feed1, dist1;
-  float mix2, speed2, feed2, dist2;
-  float drymixParam, mix1param, mix2param, speed1param, speed2param;
-  float fQuality, fTomsound;
-  float fSpeed1mode, fSpeed2mode;	// these are just for the GUI
+  double speed1, speed2;
+  float drymix;
+  float mix1, feed1, dist1;
+  float mix2, feed2, dist2;
   long quality;
   bool tomsound;
 
   int writer;
   double read1, read2;
 
-  float * buf1[2];
-  float * buf2[2];
+  float * buf1;
+  float * buf2;
   int MAXBUF;	// the size of the audio buffer (dependant on sampling rate)
 
   IIRfilter *filter1, *filter2;
   bool speed1hasChanged, speed2hasChanged;
 
-  int smoothcount1[2], smoothcount2[2], smoothdur1[2], smoothdur2[2];
-  float smoothstep1[2], smoothstep2[2], lastr1val[2], lastr2val[2];
-
-  float SAMPLERATE;
+  int smoothcount1, smoothcount2;
+  int smoothdur1, smoothdur2;
+  float smoothstep1, smoothstep2;
+  float lastr1val, lastr2val;
 
   float *firCoefficients1, *firCoefficients2;
 };
 
-#define FPARAM(pname, idx, nm, init, un) do { pname = (init); paramptrs[idx].ptr = &pname; paramptrs[idx].name = (nm); paramptrs[idx].units = (un); } while (0)
+
+
+class Transverb : public DfxPlugin {
+
+public:
+  Transverb(TARGET_API_BASE_INSTANCE_TYPE inInstance);
+  virtual ~Transverb();
+
+  virtual long initialize();
+  virtual void cleanup();
+
+  virtual bool loadpreset(long index);	// overriden to support the random preset
+// the GUI needs to be able to look at these, so they are public
+  void randomizeParameters(bool writeAutomation = false);
+
+private:
+  void initPresets();
+
+  long speed1mode, speed2mode;	// these are just for the GUI
+};
 
 
 inline float interpolateHermite (float *data, double address, 
