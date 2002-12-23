cfc40efd27c38c37a7ed39aec6ac5ef9d5bef72a
tom7 <tom7@b413c13a-479c-4ba3-9490-20a46969969a>
2002-12-23 02:07:59 +0000

    new windowingstub with dfxplugin

--- trunk/windowingstub/win32/make.bat
+++ trunk/windowingstub/win32/make.bat
@@ -1,2 +1,18 @@
+@echo off
 
-cl /nologo /O2 /Ot /Og /Oi /Oy /Gs /GD /I..\vstsdk\ /LD ..\windowingstub\windowingstub.cpp ..\vstsdk\AudioEffect.cpp ..\vstsdk\audioeffectx.cpp windowingstub.def /Fec:\progra~1\steinberg\vstplugins\dfx-windowingstub.dll
+rem    To use this, run c:\progra~1\micros~2\vc98\bin\vcvars32.bat 
+rem    (or wherever that file is located).
+
+echo -------------- resources.
+
+rem    You might want to compile some resources, if your plugin has a GUI.
+
+rem rc something.rc
+
+echo -------------- compile.
+
+cl  /FD  /MD /nologo /O2 /Ot /Og /Oi /Oy /Gs /GD /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /I..\windowingstub\ /I..\vstsdk\ /I..\dfx-library\ /LD /D "TARGET_API_VST" /FI "..\windowingstub\windowingstubdefs.h" /D "VST_NUM_CHANNELS=2" ..\windowingstub\windowingstub.cpp ..\vstsdk\audioeffect.cpp ..\vstsdk\audioeffectx.cpp ..\dfx-library\dfxplugin.cpp ..\dfx-library\dfxparameter.cpp ..\dfx-library\dfxsettings.cpp ..\dfx-library\dfxmidi.cpp ..\dfx-library\dfxplugin-vst.cpp /c
+
+echo -------------- link.
+
+link kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /incremental:no /pdb:"windowingstub.pdb" /machine:I386 /def:".\windowingstub.def" /out:"C:\Progra~1\Steinberg\VstPlugIns\dfx Windowingstub.dll" /implib:"windowingstub.lib" audioeffect.obj audioeffectx.obj windowingstub.obj  dfxplugin.obj dfxplugin-vst.obj dfxparameter.obj dfxsettings.obj dfxmidi.obj ..\vstsdk_win32\vstgui.lib
--- trunk/windowingstub/windowingstub.cpp
+++ trunk/windowingstub/windowingstub.cpp
@@ -1,1 +1,276 @@
-/* Super Destroy FX Windowing System! */#include "windowingstub.hpp"#include <stdlib.h>#include <stdio.h>#include <string.h>#include <math.h>#define MKBUFSIZE(c) (buffersizes[(int)((c)*(BUFFERSIZESSIZE-1))])const int PLUGIN::buffersizes[BUFFERSIZESSIZE] = {   2, 4, 8, 16, 32, 64, 128, 256, 512,   1024, 2048, 4096, 8192, 16384, 32768, };PLUGIN::PLUGIN(audioMasterCallback audioMaster)  : AudioEffectX(audioMaster, NUM_PROGRAMS, NUM_PARAMS) {  FPARAM(bufsizep, P_BUFSIZE, "wsize", 0.5f, "samples");  FPARAM(shape, P_SHAPE, "shape", 0.0f, "");  long maxframe = 0;  for (long i=0; i<BUFFERSIZESSIZE; i++)    maxframe = ( buffersizes[i] > maxframe ? buffersizes[i] : maxframe );  setup();  in0 = (float*)malloc(maxframe * sizeof (float));  out0 = (float*)malloc(maxframe * 2 * sizeof (float));  /* prevmix is only a single third long */  prevmix = (float*)malloc((maxframe / 2) * sizeof (float));  changed = 0;}PLUGIN::~PLUGIN() {  free (in0);  free (out0);  free (prevmix);  if (programs) delete[] programs;}void PLUGIN::resume() {  framesize = MKBUFSIZE(bufsizep);  third = framesize / 2;  bufsize = third * 3;  /* set up buffers. Prevmix and first frame of output are always filled with zeros. */  for (int i = 0; i < third; i ++) {    prevmix[i] = 0.0f;  }  for (int j = 0; j < framesize; j ++) {    out0[j] = 0.0f;  }    /* start input at beginning. Output has a frame of silence. */  insize = 0;  outstart = 0;  outsize = framesize;  setInitialDelay(framesize);  changed = 0;  needIdle();}void PLUGIN::suspend () {  /* nothing to do here. */}long PLUGIN::fxIdle() {  /* ioChanged() causes resume() to be called and      changed to initialDelay are updated for the host */  if (changed)    ioChanged();  changed = 0;  return 1;}/* tail is the same as delay, of course */long PLUGIN::getTailSize() { return framesize; }void PLUGIN::setParameter(long index, float value) {  switch (index) {  case P_BUFSIZE:    changed = 1;    /* fallthrough */  default:    if (index >= 0 && index < NUM_PARAMS)      *paramptrs[index].ptr = value;    /* otherwise, ??? */    break;  }  /* copy the new value to the active program's corresponding parameter value */  if ( (index >= 0) && (index < NUM_PARAMS) )    programs[curProgram].param[index] = value;}float PLUGIN::getParameter(long index) {  switch (index) {    /* special cases here */  default:    /* otherwise pull it out of array. */    if (index >= 0 && index < NUM_PARAMS) return *paramptrs[index].ptr;    else return 0.0f; /* ? */  }}void PLUGIN::getParameterName(long index, char *label) {  switch(index) {    /* special cases here */  default:    if (index >= 0 && index < NUM_PARAMS && paramptrs[index].name)       strcpy(label, paramptrs[index].name);    else strcpy(label, "?");    break;  }}void PLUGIN::getParameterDisplay(long index, char *text) {  switch(index) {  case P_BUFSIZE:    sprintf(text, "%d", MKBUFSIZE(bufsizep));    break;  case P_SHAPE:    switch (wShapeScaled(shape)) {      case W_SQRT:        strcpy(text, "linear");        break;      case W_SQUARED:        strcpy(text, "arrow");        break;      case W_LINEAR:        strcpy(text, "wedge");        break;      case W_COS:        strcpy(text, "best");        break;      case W_COS_SQUARED:        strcpy(text, "cos^2");        break;    }    break;    /* special cases here */  default:    float2string(getParameter(index), text);    break;  }}void PLUGIN::getParameterLabel(long index, char *label) {  switch(index) {    /* special cases here */  default:    if (index >= 0 && index < NUM_PARAMS && paramptrs[index].units)       strcpy(label, paramptrs[index].units);    else strcpy(label, "?");    break;  }}/* this is where you should write your real processing   function. It will be called on overlapping input frames,   but it doesn't need to know about that.   Keep in mind that, for instance, a delay-like plug   that keeps a history of samples it's seen will not work   right here, since processw does not receive adjacent   frames. It might be possible to use two delay buffers,   toggling between them with each call, but it's probably   best to just use the windowing setup for functions that   operate entirely within one buffer.*/void PLUGIN::processw(float * in, float * out, long samples) {  static int ss;  static int t;  ss = !ss;#if 0  /*  for (int i =0 ; i < samples; i++) out[i] = (float)ss; */  for (int i = 0; i < samples; i ++) out[i] = in[i];#else#if 0  /* test with sine tones */  if (ss) {    for(int i = 0; i < samples; i ++) {      t++;      out[i] = sinf(t/110.0f);    }  } else {    for(int i = 0; i < samples; i ++) {      t++;      out[i] = sinf(t/37.0f);    }  }#else  /* reverse */  for(int i = 0; i < samples; i ++) out[i] = in[samples - (i + 1)];#endif#endif}/* this fake processX function reads samples one at a time   from the true input. It simultaneously copies samples from   the beginning of the output buffer to the true output.   We maintain that out0 always has at least 'third' samples   in it; this is enough to pick up for the delay of input   processing and to make sure we always have enough samples   to fill the true output buffer.   If the input frame is full:    - calls wprocess on this full input frame    - applies the windowing envelope to the tail of out0 (output frame)    - mixes in prevmix with the first half of the output frame    - increases outsize so that the first half of the output frame is      now available output    - copies the second half of the output to be prevmix for next frame.    - copies the second half of the input buffer to the first,      resets the size (thus we process each third-size chunk twice)  If we have read more than 'third' samples out of the out0 buffer:   - Slide contents to beginning of buffer   - Reset outstart*//* to improve:    - use memcpy and arithmetic instead of     sample-by-sample copy    - can we use tail of out0 as prevmix, instead of copying?   - can we use circular buffers instead of memmoving a lot     (probably not)*/void PLUGIN::processX(float **trueinputs, float **trueoutputs, long samples, 		      int replacing) {  float * tin  = *trueinputs;  float * tout = *trueoutputs;  for (int ii = 0; ii < samples; ii++) {    /* copy sample in */    in0[insize] = tin[ii];    insize ++;     if (insize == framesize) {      /* frame is full! */      /* in0 -> process -> out0(first free space) */      processw(in0, out0+outstart+outsize, framesize);      float oneDivThird = 1.0f / (float)third;      /* apply envelope */      switch (wShapeScaled(shape)) {        case W_SQRT:          for(int z = 0; z < third; z++) {            float p = sqrtf((float)z * oneDivThird);            out0[z+outstart+outsize] *= p;            out0[z+outstart+outsize+third] *= (1.0f - p);          }          break;        case W_SQUARED:          for(int z = 0; z < third; z++) {            float p = (float)z * oneDivThird;            p *= p;            out0[z+outstart+outsize] *= p;            out0[z+outstart+outsize+third] *= (1.0f - p);          }          break;        case W_LINEAR:          for(int z = 0; z < third; z++) {            float p = (float)z * oneDivThird;            out0[z+outstart+outsize] *= p;            out0[z+outstart+outsize+third] *= (1.0f - p);          }          break;        case W_COS:          for(int z = 0; z < third; z ++) {            float p = 0.5f * (1.0f - cosf(pi * ((float)z * oneDivThird)));            out0[z+outstart+outsize] *= p;            out0[z+outstart+outsize+third] *= (1.0f - p);          }          break;        case W_COS_SQUARED:          for(int z = 0; z < third; z ++) {            float p = 0.5f * (1.0f - cosf(pi * ((float)z * oneDivThird)));            p = p * p;            out0[z+outstart+outsize] *= p;            out0[z+outstart+outsize+third] *= (1.0f - p);          }          break;      }      /* mix in prevmix */      for(int u = 0; u < third; u ++)	out0[u+outstart+outsize] += prevmix[u];      /* prevmix becomes out1 */      memcpy(prevmix, out0 + outstart + outsize + third, third * sizeof (float));      /* copy 2nd third of input over in0 (need to re-use it for next frame), 	 now insize = third */      memcpy(in0, in0 + third, third * sizeof (float));      insize = third;            outsize += third;    }    /* send sample out */    if (replacing) tout[ii] = out0[outstart];    else tout[ii] += out0[outstart];    outstart ++;    outsize --;    /* make sure there is always enough room for a frame in out buffer */    if (outstart == third) {      memmove(out0, out0 + outstart, outsize * sizeof (float));      outstart = 0;    }  }}/* these should always call the common processX function */void PLUGIN::processReplacing(float **inputs, float **outputs, long samples) {  processX(inputs,outputs,samples, 1);}void PLUGIN::process(float **inputs, float **outputs, long samples) {  processX(inputs,outputs,samples, 0);}/* program stuff. Should never need to mess with this, hopefully.. */void PLUGINPROGRAM::init(PLUGIN * p) {  /* copy defaults from paramptrs */  for (int i=0; i < NUM_PARAMS; i++)    param[i] = p->paramptrs[i].def;}void PLUGIN::setProgram(long programNum) {  if ( (programNum < NUM_PROGRAMS) && (programNum >= 0) ) {      AudioEffectX::setProgram(programNum);      curProgram = programNum;      for (int i=0; i < NUM_PARAMS; i++)        setParameter(i, programs[programNum].param[i]);    }  // tell the host to update the editor display with the new settings  AudioEffectX::updateDisplay();}void PLUGIN::setProgramName(char *name) {  strcpy(programs[curProgram].name, name);}void PLUGIN::getProgramName(char *name) {  if ( !strcmp(programs[curProgram].name, "default") )    sprintf(name, "default %ld", curProgram+1);  else    strcpy(name, programs[curProgram].name);}bool PLUGIN::getProgramNameIndexed(long category, long index, char * text) {  if ( (index < NUM_PROGRAMS) && (index >= 0) ) {    strcpy(text, programs[index].name);    return true;  }  return false;}bool PLUGIN::copyProgram(long destination) {  if ( (destination < NUM_PROGRAMS) && (destination >= 0) ) {    programs[destination] = programs[curProgram];    return true;  }  return false;}/* this is only compiled if not building the GUI version */#ifndef GUI/* XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX */ /* ---------- boring stuff below this line ----------- *//* XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX */ static AudioEffect *effect = 0;bool oome = false;#if MAC   #pragma export on#endif// prototype of the export function main#if BEOS   #define main main_plugin   extern "C" __declspec(dllexport) AEffect       *main_plugin (audioMasterCallback audioMaster);#else   AEffect *main (audioMasterCallback audioMaster);#endifAEffect *main (audioMasterCallback audioMaster) {  // get vst version  if (!audioMaster (0, audioMasterVersion, 0, 0, 0, 0))    return 0;  // old version  effect = new PLUGIN (audioMaster);  if (!effect)    return 0;  if (oome) {    delete effect;    return 0;  }  return effect->getAeffect ();}#if MAC  #pragma export off#endif#if WIN32  void* hInstance;  BOOL WINAPI DllMain (HINSTANCE hInst, DWORD dwReason, LPVOID lpvReserved) {    hInstance = hInst;    return 1;  }#endif#endif
+
+/* Windowingstub,
+   Featuring the Super Destroy FX Windowing System! */
+
+#include "windowingstub.h"
+
+#include <stdio.h>
+
+#if defined(TARGET_API_VST) && TARGET_PLUGIN_HAS_GUI
+  #ifndef __DFX_WINDOWINGSTUBEDITOR_H
+  #include "windowingstubeditor.hpp"
+  #endif
+#endif
+
+/* this macro does boring entry point stuff for us */
+DFX_ENTRY(Windowingstub);
+#if TARGET_PLUGIN_USES_DSPCORE
+  DFX_CORE_ENTRY(WindowingstubDSP);
+#endif
+
+PLUGIN::PLUGIN(TARGET_API_BASE_INSTANCE_TYPE inInstance)
+  : DfxPlugin(inInstance, NUM_PARAMS, NUM_PRESETS) {
+
+  initparameter_indexed(P_BUFSIZE, "wsize", 9, 9, BUFFERSIZESSIZE, kDfxParamUnit_samples);
+  initparameter_indexed(P_SHAPE, "wshape", WINDOW_TRIANGLE, WINDOW_TRIANGLE, MAX_WINDOWSHAPES);
+
+  long i;
+  /* set up values for windowing */
+  char bufstr[64];
+  for (i=0; i < BUFFERSIZESSIZE; i++) {
+    if (buffersizes[i] > 1000)
+      sprintf(bufstr, "%ld,%03ld", buffersizes[i]/1000, buffersizes[i]%1000);
+    else
+      sprintf(bufstr, "%ld", buffersizes[i]);
+    setparametervaluestring(P_BUFSIZE, i, bufstr);
+  }
+
+  setparametervaluestring(P_SHAPE, WINDOW_TRIANGLE, "linear");
+  setparametervaluestring(P_SHAPE, WINDOW_ARROW, "arrow");
+  setparametervaluestring(P_SHAPE, WINDOW_WEDGE, "wedge");
+  setparametervaluestring(P_SHAPE, WINDOW_COS, "best");
+  for (i=NUM_WINDOWSHAPES; i < MAX_WINDOWSHAPES; i++)
+    setparametervaluestring(P_SHAPE, i, "???");
+
+  long delay_samples = buffersizes[getparameter_i(P_BUFSIZE)];
+  setlatency_samples(delay_samples);
+  settailsize_samples(delay_samples);
+
+  setpresetname(0, "Windowingstub Default"); /* default preset name */
+  makepresets();
+
+  /* allow MIDI keys to be used to control parameters */
+  dfxsettings->setAllowPitchbendEvents(true);
+  dfxsettings->setAllowNoteEvents(true);
+
+#if !TARGET_PLUGIN_USES_DSPCORE
+  addchannelconfig(1, 1);	/* mono */
+#endif
+
+  #ifdef TARGET_API_VST
+    #if TARGET_PLUGIN_USES_DSPCORE
+      DFX_INIT_CORE(WindowingstubDSP);	/* we need to manage DSP cores manually in VST */
+    #endif
+    /* if you have a GUI, need an Editor class... */
+    #if TARGET_PLUGIN_HAS_GUI
+      editor = new WindowingstubEditor(this);
+    #endif
+  #endif
+}
+
+PLUGIN::~PLUGIN() {
+
+#ifdef TARGET_API_VST
+  /* VST doesn't have initialize and cleanup methods like Audio Unit does, 
+    so we need to call this manually here */
+  do_cleanup();
+#endif
+}
+
+PLUGINCORE::PLUGINCORE(TARGET_API_CORE_INSTANCE_TYPE *inInstance)
+  : DfxPluginCore(inInstance) {
+  /* determine the size of the largest window size */
+  long maxframe = 0;
+  for (int i = 0; i < BUFFERSIZESSIZE; i++)
+    maxframe = (buffersizes[i] > maxframe) ? buffersizes[i] : maxframe;
+
+  /* add some leeway? */
+  in0 = (float*)malloc(maxframe * sizeof (float));
+  out0 = (float*)malloc(maxframe * 2 * sizeof (float));
+
+  /* prevmix is only a single third long */
+  prevmix = (float*)malloc((maxframe / 2) * sizeof (float));
+}
+
+
+PLUGINCORE::~PLUGINCORE() {
+  /* windowing buffers */
+  free (in0);
+  free (out0);
+
+  free (prevmix);
+}
+
+void PLUGINCORE::reset() {
+
+  framesize = buffersizes[getparameter_i(P_BUFSIZE)];
+  third = framesize / 2;
+  bufsize = third * 3;
+
+  /* set up buffers. Prevmix and first frame of output are always 
+     filled with zeros. XXX memset */
+
+  for (int i = 0; i < third; i ++) {
+    prevmix[i] = 0.0f;
+  }
+
+  for (int j = 0; j < framesize; j ++) {
+    out0[j] = 0.0f;
+  }
+  
+  /* start input at beginning. Output has a frame of silence. */
+  insize = 0;
+  outstart = 0;
+  outsize = framesize;
+
+  dfxplugin->setlatency_samples(framesize);
+  /* tail is the same as delay, of course */
+  dfxplugin->settailsize_samples(framesize);
+}
+
+void PLUGINCORE::processparameters() {
+
+  shape = getparameter_i(P_SHAPE);
+
+  #ifdef TARGET_API_VST
+    /* this tells the host to call a suspend()-resume() pair, 
+      which updates initialDelay value */
+  if (getparameterchanged(P_BUFSIZE))
+    dfxplugin->setlatencychanged(true);
+  #endif
+}
+
+/* this processes an individual window. Basically, this is where you
+   write your DSP, and it will be always called with the same sample
+   size (as long as the block size parameter stays the same) and
+   automatically overlapped. */
+void PLUGINCORE::processw(float * in, float * out, long samples) {
+
+  memmove(out, in, samples * sizeof (float));
+
+}
+
+
+/* this fake process function reads samples one at a time
+   from the true input. It simultaneously copies samples from
+   the beginning of the output buffer to the true output.
+   We maintain that out0 always has at least 'third' samples
+   in it; this is enough to pick up for the delay of input
+   processing and to make sure we always have enough samples
+   to fill the true output buffer.
+
+   If the input frame is full:
+    - calls wprocess on this full input frame
+    - applies the windowing envelope to the tail of out0 (output frame)
+    - mixes in prevmix with the first half of the output frame
+    - increases outsize so that the first half of the output frame is
+      now available output
+    - copies the second half of the output to be prevmix for next frame.
+    - copies the second half of the input buffer to the first,
+      resets the size (thus we process each third-size chunk twice)
+
+  If we have read more than 'third' samples out of the out0 buffer:
+   - Slide contents to beginning of buffer
+   - Reset outstart
+
+*/
+
+/* to improve: 
+   - use memcpy and arithmetic instead of
+     sample-by-sample copy 
+   - can we use tail of out0 as prevmix, instead of copying?
+   - can we use circular buffers instead of memmoving a lot?
+     (probably not)
+*/
+
+
+void PLUGINCORE::process(const float *tin, float *tout, unsigned long samples, bool replacing) {
+  int z = 0;
+
+  for (unsigned long ii = 0; ii < samples; ii++) {
+
+    /* copy sample in */
+    in0[insize] = tin[ii];
+    insize ++;
+ 
+    if (insize == framesize) {
+      /* frame is full! */
+
+      /* in0 -> process -> out0(first free space) */
+      processw(in0, out0+outstart+outsize, framesize);
+
+      float oneDivThird = 1.0f / (float)third;
+      /* apply envelope */
+
+      switch(shape) {
+
+        case WINDOW_TRIANGLE:
+          for(z = 0; z < third; z++) {
+            out0[z+outstart+outsize] *= ((float)z * oneDivThird);
+            out0[z+outstart+outsize+third] *= (1.0f - ((float)z * oneDivThird));
+          }
+          break;
+        case WINDOW_ARROW:
+          for(z = 0; z < third; z++) {
+            float p = (float)z * oneDivThird;
+            p *= p;
+            out0[z+outstart+outsize] *= p;
+            out0[z+outstart+outsize+third] *= (1.0f - p);
+          }
+          break;
+        case WINDOW_WEDGE:
+          for(z = 0; z < third; z++) {
+            float p = sqrtf((float)z * oneDivThird);
+            out0[z+outstart+outsize] *= p;
+            out0[z+outstart+outsize+third] *= (1.0f - p);
+          }
+          break;
+        case WINDOW_COS:
+          for(z = 0; z < third; z ++) {
+            float p = 0.5f * (-cosf(PI * ((float)z * oneDivThird)) + 1.0f);
+            out0[z+outstart+outsize] *= p;
+            out0[z+outstart+outsize+third] *= (1.0f - p);
+          }
+          break;
+      }
+
+      /* mix in prevmix */
+      for(int u = 0; u < third; u ++)
+        out0[u+outstart+outsize] += prevmix[u];
+
+      /* prevmix becomes out1 */
+      memcpy(prevmix, out0 + outstart + outsize + third, third * sizeof (float));
+
+      /* copy 2nd third of input over in0 (need to re-use it for next frame), 
+         now insize = third */
+      memcpy(in0, in0 + third, third * sizeof (float));
+
+      insize = third;
+      
+      outsize += third;
+    }
+
+    /* send sample out */
+  #ifdef TARGET_API_VST
+    if (replacing)
+  #endif
+      tout[ii] = out0[outstart];
+  #ifdef TARGET_API_VST
+    else tout[ii] += out0[outstart];
+  #endif
+
+    outstart ++;
+    outsize --;
+
+    /* make sure there is always enough room for a frame in out buffer */
+    if (outstart == third) {
+      memmove(out0, out0 + outstart, outsize * sizeof (float));
+      outstart = 0;
+    }
+  }
+}
+
+
+void PLUGIN::makepresets() {
+  /* initialize presets here, see geometer for an example. */
+}
