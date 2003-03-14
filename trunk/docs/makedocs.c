032578161e24b585492b24e5c1e86621030f86c8
marc3 <marc3@b413c13a-479c-4ba3-9490-20a46969969a>
2003-03-14 23:49:40 +0000

    now supports multi-line <link> tags, and added I more comments

--- trunk/docs/makedocs.c
+++ trunk/docs/makedocs.c
@@ -13,24 +13,30 @@
  *  dumps the contents of any linked files in place of the 
  *  <link> tags.
  *
- *  usage:  The first argument is the input file name 
+ *  USAGE:  The first argument is the input file name 
  *  (i.e. the file that has <link> tags) and the second 
  *  argument is the consolidated output file name.  
  *  The second argument is optional (stdout is used by default).
  *
- *  BUGS:  If the <link> tag spans more than one line, 
- *  evenything past the first line will be dumped to output.
+ *  BUGS:  If there is a legitimate (for example, part of a 
+ *  attribute value that is in quotes) > character after the 
+ *  href value inside of a link tag, the HTML parser will be 
+ *  fooled into thinking that that is the end of the link tag.
  */
+
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>	/* for isspace */
 #include <libgen.h>	/* for basename and dirname */
+#include <errno.h>	/* for error codes */
 
 
 #define LINK_TAG	"link"
 #define LINK_SOURCE	"href="
-
+#define CVS_ENTRIES_FILE	"CVS/Entries"
+#define CVS_ROOT_FILE	"CVS/Root"
+#define CVS_REPOSITORY_FILE	"CVS/Repository"
 
 enum {
 	kArg_Command = 0,
@@ -39,6 +45,7 @@ enum {
 
 	kNumArgs
 };
+
 
 // hmmm, strcasestr doesn't appear to be included with BSD or Darwin...
 char * dfx_strcasestr(const char *big, const char *little)
@@ -56,6 +63,175 @@ char * dfx_strcasestr(const char *big, const char *little)
 }
 
 
+// try to use CVS logged data to construct an ultra-informative file header section
+void writeinfoheader(FILE *outputfile, const char *inputfilename)
+{
+	// we're going to look for a CVS directory in the same 
+	// directory where the input file is located
+	char *sourcedir = dirname(inputfilename);
+
+	// get full path and filename for the CVS Entries file
+	char cvsentriesfilefullpath[strlen(sourcedir) + strlen(CVS_ENTRIES_FILE) + 4];
+	sprintf(cvsentriesfilefullpath, "%s/%s", sourcedir, CVS_ENTRIES_FILE);
+	// open the Entries file
+	FILE *cvsentriesf = fopen(cvsentriesfilefullpath, "r");
+	if (cvsentriesf != NULL)
+	{
+		// get the filename of the input file so that we 
+		// can look for it in the CVS Entries log
+		char *sourcefilebase = basename(inputfilename);
+		size_t sourcefilebaselen = strlen(sourcefilebase);
+
+		// loop through reading each line of the Entries file
+		while ( !feof(cvsentriesf) )
+		{
+			// read the input file one line at a time
+			size_t linesize;
+			char *linestr = fgetln(cvsentriesf, &linesize);
+			// try the next line if this one failed
+			if ( (linestr == NULL) || (linesize <= (sourcefilebaselen+1)) )
+				continue;
+
+			// values in each line are separated by / characters
+			// look for the first / to find where the first value begins
+			long linepos = 0;
+			while (linepos < (linesize-1))
+			{
+				if (linestr[linepos] == '/')
+					break;
+				linepos++;
+			}
+			linepos++;	// advance past the / delimiting character
+
+			// check if the first value, the filename, matches our input filename
+			if (strncmp(linestr+linepos, sourcefilebase, sourcefilebaselen) != 0)
+				continue;	// move on to the next line if it doesn't match
+
+			// we found a match, so advance past the filename value in the line
+			linepos += sourcefilebaselen;
+			while ( (linepos < (linesize-1)) && (linestr[linepos] != '/') )
+				linepos++;
+			linepos++;	// advance past the / delimiting character
+
+			// the next value is the latest version number of the file
+			char versionstr[linesize-linepos];
+			long versionstrpos = 0;
+			// copy the version value into our version string until a / is reached
+			while ( (linepos < (linesize-1)) && (linestr[linepos] != '/') )
+			{
+				versionstr[versionstrpos] = linestr[linepos];
+				linepos++;
+				versionstrpos++;
+			}
+			versionstr[versionstrpos] = 0;	// terminate the string
+			linepos++;	// advance past the / delimiting character
+
+			// the next value in the line is the last file commit date and time
+			char datestr[linesize-linepos];
+			long datestrpos = 0;
+			// copy the date and time value into our date and time string until a / is reached
+			while ( (linepos < (linesize-1)) && (linestr[linepos] != '/') )
+			{
+				datestr[datestrpos] = linestr[linepos];
+				linepos++;
+				datestrpos++;
+			}
+			datestr[datestrpos] = 0;	// terminate the string
+
+
+		// Root file
+			// next we will try to retrive the CVS Root from the Root file
+			char cvsrootfilefullpath[strlen(sourcedir) + strlen(CVS_ROOT_FILE) + 4];
+			sprintf(cvsrootfilefullpath, "%s/%s", sourcedir, CVS_ROOT_FILE);
+			char cvsrootpathstr[1024];	// for storing the Root string
+			cvsrootpathstr[0] = 0;
+			// see if we can find the CVS Root file
+			FILE *cvsrootf = fopen(cvsrootfilefullpath, "r");
+			if (cvsrootf != NULL)
+			{
+				// first find the @ character 
+				// (we're not interested in login name and type, 
+				// so we want to begin the path after the @)
+				while (!feof(cvsrootf))
+				{
+					int got = fgetc(cvsrootf);
+					if ( (got == EOF) || (got == '@') || iscntrl(got) )
+						break;
+				}
+
+				// now copy up until the : character 
+				// (beyond that is partially the repository path)
+				long strpos = 0;
+				while (!feof(cvsrootf))
+				{
+					int got = fgetc(cvsrootf);
+					if ( (got == EOF) || (got == ':') || iscntrl(got) )
+						break;
+					cvsrootpathstr[strpos] = got;
+					strpos++;
+				}
+				cvsrootpathstr[strpos] = 0;	// terminate the string
+				// if we managed to get anything, then 
+				// append the : to the root path string
+				if (strlen(cvsrootpathstr) > 0)
+					strcat(cvsrootpathstr, ":");
+
+				fclose(cvsrootf);
+			}
+
+		// Repository file
+			// then we will try to retrive the CVS Repository from the Repository file
+			char cvsrepositoryfilefullpath[strlen(sourcedir) + strlen(CVS_REPOSITORY_FILE) + 4];
+			sprintf(cvsrepositoryfilefullpath, "%s/%s", sourcedir, CVS_REPOSITORY_FILE);
+			char cvsrepositorypathstr[1024];	// for storing the Repository string
+			cvsrepositorypathstr[0] = 0;
+			// see if we can find the CVS Repository file
+			FILE *cvsrepositoryf = fopen(cvsrepositoryfilefullpath, "r");
+			if (cvsrepositoryf != NULL)
+			{
+				// copy the entire line from the Repository file
+				long strpos = 0;
+				while (!feof(cvsrootf))
+				{
+					int got = fgetc(cvsrepositoryf);
+					if ( (got == EOF) || iscntrl(got) )
+						break;
+					cvsrepositorypathstr[strpos] = got;
+					strpos++;
+				}
+				cvsrepositorypathstr[strpos] = 0;	// terminate the string
+				// if we managed to get anything, then 
+				// append the / to the repository path string
+				if (strlen(cvsrepositorypathstr) > 0)
+					strcat(cvsrepositorypathstr, "/");
+
+				fclose(cvsrepositoryf);
+			}
+
+			// print a commented-out info blurb at the beginning of the HTML output stream
+			fprintf(outputfile, "<!--\n");
+			fprintf(outputfile, "\tthis file generated from:  %s%s%s\n", 
+					cvsrootpathstr, cvsrepositorypathstr, sourcefilebase);
+			// only print version info if we managed to get any
+			if (strlen(versionstr) > 0)
+				fprintf(outputfile, "\tfile version:  %s\n", versionstr);
+			// only print date and time info if we managed to get any
+			if (strlen(datestr) > 0)
+				fprintf(outputfile, "\tlast modified:  %s\n", datestr);
+			fprintf(outputfile, "-->\n\n");
+
+			// if we made it this far, then we found our CVS entry info 
+			// and don't need to loop through the file any more
+			break;
+		}
+		// end of while loop reading CVS Entries file
+
+		fclose(cvsentriesf);
+	}
+	// end of if CVS Entries file is valid statement
+}
+
+
 
 int main(int argc, char **argv)
 {
@@ -64,7 +240,7 @@ int main(int argc, char **argv)
 	if (argc < (kNumArgs-1))
 	{
 		printf("\t%s inputfile [outputfile]\n", basename(argv[kArg_Command]));
-		return 1;
+		return EINVAL;
 	}
 
 	// try to open the input file
@@ -72,7 +248,7 @@ int main(int argc, char **argv)
 	if (sourcef == NULL)
 	{
 		printf("could not open input file\n");
-		return 2;
+		return ENOENT;
 	}
 
 	// if an output file was specified, try to open it, otherwise use stdout
@@ -83,134 +259,15 @@ int main(int argc, char **argv)
 		if (destf == NULL)
 		{
 			printf("could not create output file %s\n", argv[kArg_OutputFile]);
-			return 3;
+			return EIO;
 		}
 	}
 
-	char *sourcedir = dirname(argv[kArg_InputFile]);
-	// get full path and filename for the CVS Entries file
-#define CVS_ENTRIES_FILE	"CVS/Entries"
-	char cvsentriesfilefullpath[strlen(sourcedir) + strlen(CVS_ENTRIES_FILE) + 4];
-	sprintf(cvsentriesfilefullpath, "%s/%s", sourcedir, CVS_ENTRIES_FILE);
-#undef CVS_ENTRIES_FILE
-	// open the linked file
-	FILE *cvsentriesf = fopen(cvsentriesfilefullpath, "r");
-	if (cvsentriesf != NULL)
-	{
-		char *sourcefilebase = basename(argv[kArg_InputFile]);
-		size_t sourcefilebaselen = strlen(sourcefilebase);
-
-		while ( !feof(cvsentriesf) )
-		{
-			// read the input file one line at a time
-			size_t linesize;
-			char *linestr = fgetln(cvsentriesf, &linesize);
-			// try the next line if this one failed
-			if ( (linestr == NULL) || (linesize <= (sourcefilebaselen+1)) )
-				continue;
-			long linepos = 0;
-			while (linepos < (linesize-1))
-			{
-				if (linestr[linepos] == '/')
-					break;
-				linepos++;
-			}
-			linepos++;	// move past the / delimiting character
-
-			if (strncmp(linestr+1, sourcefilebase, sourcefilebaselen) != 0)
-				continue;
-			linepos = sourcefilebaselen + 1;
-			while ( (linepos < (linesize-1)) && (linestr[linepos] != '/') )
-				linepos++;
-
-			linepos++;	// move past the / delimiting character
-			char versionstr[linesize-linepos];
-			long versionstrpos = 0;
-			while ( (linepos < (linesize-1)) && (linestr[linepos] != '/') )
-			{
-				versionstr[versionstrpos] = linestr[linepos];
-				linepos++;
-				versionstrpos++;
-			}
-			versionstr[versionstrpos] = 0;	// terminate the string
-
-			linepos++;	// move past the / delimiting character
-			char datestr[linesize-linepos];
-			long datestrpos = 0;
-			while ( (linepos < (linesize-1)) && (linestr[linepos] != '/') )
-			{
-				datestr[datestrpos] = linestr[linepos];
-				linepos++;
-				datestrpos++;
-			}
-			datestr[datestrpos] = 0;	// terminate the string
-
-		#define CVS_ROOT_FILE	"CVS/Root"
-			char cvsrootfilefullpath[strlen(sourcedir) + strlen(CVS_ROOT_FILE) + 4];
-			sprintf(cvsrootfilefullpath, "%s/%s", sourcedir, CVS_ROOT_FILE);
-		#undef CVS_ROOT_FILE
-			char cvsrootpathstr[1024];
-			cvsrootpathstr[0] = 0;
-			FILE *cvsrootf = fopen(cvsrootfilefullpath, "r");
-			if (cvsrootf != NULL)
-			{
-				size_t linesize2;
-				char *linestr2 = fgetln(cvsrootf, &linesize2);
-				if ( (linestr2 != NULL) || (linesize2 > 0) )
-				{
-					long linepos2 = 0;
-					while ( (linepos2 < (linesize2-1)) && (linestr2[linepos2] != '@') )
-						linepos2++;
-					linepos2++;	// move past the @ character
-
-					long rootstrpos = 0;
-					while ( (linepos2 < (linesize2-1)) && (linestr2[linepos2] != ':') )
-					{
-						cvsrootpathstr[rootstrpos] = linestr2[linepos2];
-						linepos2++;
-						rootstrpos++;
-					}
-					cvsrootpathstr[rootstrpos] = 0;	// terminate the string
-					strcat(cvsrootpathstr, ":");
-				}
-				fclose(cvsrootf);
-			}
-
-		#define CVS_REPOSITORY_FILE	"CVS/Repository"
-			char cvsrepositoryfilefullpath[strlen(sourcedir) + strlen(CVS_REPOSITORY_FILE) + 4];
-			sprintf(cvsrepositoryfilefullpath, "%s/%s", sourcedir, CVS_REPOSITORY_FILE);
-		#undef CVS_REPOSITORY_FILE
-			char cvsrepositorypathstr[1024];
-			cvsrepositorypathstr[0] = 0;
-			FILE *cvsrepositoryf = fopen(cvsrepositoryfilefullpath, "r");
-			if (cvsrepositoryf != NULL)
-			{
-				size_t linesize2;
-				char *linestr2 = fgetln(cvsrepositoryf, &linesize2);
-				if ( (linestr2 != NULL) || (linesize2 > 0) )
-				{
-					long linepos2;
-					for (linepos2=0; linepos2 < (linesize2-1); linepos2++)
-						cvsrepositorypathstr[linepos2] = linestr2[linepos2];
-					cvsrepositorypathstr[linepos2] = 0;	// terminate the string
-					strcat(cvsrepositorypathstr, "/");
-				}
-				fclose(cvsrepositoryf);
-			}
-
-			fprintf(destf, "<!--\n");
-			fprintf(destf, "\tthis file built from:  %s%s%s\n", cvsrootpathstr, cvsrepositorypathstr, sourcefilebase);
-			if (strlen(versionstr) > 0)
-				fprintf(destf, "\tfile version:  %s\n", versionstr);
-			if (strlen(datestr) > 0)
-				fprintf(destf, "\tlast modified:  %s\n", datestr);
-			fprintf(destf, "-->\n\n");
-		}
-
-		fclose(cvsentriesf);
-	}
+	// if possible, start by writing info about the input file (version, date, etc.)
+	writeinfoheader(destf, argv[kArg_InputFile]);
 
 	// this is the main input file reading loop
+readlineloop:
 	while ( !feof(sourcef) )
 	{
 		// read the input file one line at a time
@@ -299,13 +356,26 @@ int main(int argc, char **argv)
 								i += linksource - linestr_copy + strlen(linkfilename);
 								while (1)
 								{
+									// if a > is found in this line, then advance past it, 
+									// break out of this loop, and continue copying from 
+									// the input file to the output stream
 									if (linestr_copy[i] == '>')
 									{
 										i++;	// advance past the tag closure bracket
 										break;
 									}
+									// if the end of the line is reached, then keep grabbing 
+									// characters from the input stream until > is found, 
+									// and then return to the start of the line reading loop
 									if (linestr_copy[i] == 0)
-										break;
+									{
+										while (!feof(sourcef))
+										{
+											int got = fgetc(sourcef);
+											if ( (got == '>') || (got == EOF) )
+												goto readlineloop;
+										}
+									}
 									i++;
 								}
 							}
