f3f1b53ab859ca5453fdf7ea3227ac1e6a158f95
marc3 <marc3@b413c13a-479c-4ba3-9490-20a46969969a>
2003-03-25 20:15:55 +0000

    added new 'input selection' feature/parameter to Monomaker

--- trunk/monomaker/monomaker.cpp
+++ trunk/monomaker/monomaker.cpp
@@ -16,14 +16,21 @@ Monomaker::Monomaker(TARGET_API_BASE_INSTANCE_TYPE inInstance)
 	: DfxPlugin(inInstance, NUM_PARAMETERS, 1)	// 2 parameters, 1 preset
 {
 	// initialize the parameters
+	initparameter_indexed(kInputSelection, "input selection", kInputSelection_stereo, kInputSelection_stereo, kNumInputSelections);
 	initparameter_f(kMonomerge, "monomix", 0.0f, 100.0f, 0.0f, 100.0f, kDfxParamUnit_percent);
 	initparameter_indexed(kMonomergeMode, "monomix mode", kMonomergeMode_equalpower, kMonomergeMode_linear, kNumMonomergeModes);
 	initparameter_f(kPan, "pan", 0.0f, 0.0f, -1.0f, 1.0f, kDfxParamUnit_pan);
 	initparameter_indexed(kPanMode, "pan mode", kPanMode_recenter, kPanMode_recenter, kNumPanModes);
 
 	// set the parameter value display strings
+	setparametervaluestring(kInputSelection, kInputSelection_stereo, "left-right");
+	setparametervaluestring(kInputSelection, kInputSelection_swap, "right-left");
+	setparametervaluestring(kInputSelection, kInputSelection_left, "left-left");
+	setparametervaluestring(kInputSelection, kInputSelection_right, "right-right");
+	//
 	setparametervaluestring(kMonomergeMode, kMonomergeMode_linear, "linear");
 	setparametervaluestring(kMonomergeMode, kMonomergeMode_equalpower, "equal power");
+	//
 	setparametervaluestring(kPanMode, kPanMode_recenter, "recenter");
 	setparametervaluestring(kPanMode, kPanMode_balance, "balance");
 
@@ -41,15 +48,34 @@ Monomaker::Monomaker(TARGET_API_BASE_INSTANCE_TYPE inInstance)
 //-----------------------------------------------------------------------------------------
 void Monomaker::processaudio(const float **inputs, float **outputs, unsigned long inNumFrames, bool replacing)
 {
-	// temp input value holders
-	float in1, in2, out1, out2;
-	// input signal pointers, in order to support mono or stereo input
-	const float *input1 = inputs[0], *input2 = (getnuminputs() < 2) ? inputs[0] : inputs[1];
 	// fetch the current parameter values
+	long inputselection = getparameter_i(kInputSelection);
 	float monomerge = getparameter_scalar(kMonomerge);
 	long monomergemode = getparameter_i(kMonomergeMode);
 	float pan = getparameter_f(kPan);
 	long panmode = getparameter_i(kPanMode);
+
+	// point the input signal pointers to the correct input streams, 
+	// according to the input selection (or dual-left if we only have 1 input)
+	const float *input1, *input2;
+	if ( (inputselection == kInputSelection_left) || (getnuminputs() == 1) )
+	{
+		input1 = input2 = inputs[0];
+	}
+	else if (inputselection == kInputSelection_right)
+	{
+		input1 = input2 = inputs[1];
+	}
+	else if (inputselection == kInputSelection_swap)
+	{
+		input1 = inputs[1];
+		input2 = inputs[0];
+	}
+	else
+	{
+		input1 = inputs[0];
+		input2 = inputs[1];
+	}
 
 	// calculate monomerge gain scalars
 	float monomerge_main = 1.0f - (monomerge * 0.5f);
@@ -89,29 +115,25 @@ void Monomaker::processaudio(const float **inputs, float **outputs, unsigned lon
 	}
 
 	// process the audio streams
-	for (unsigned long samplecount=0; samplecount < inNumFrames; samplecount++)
+	for (unsigned long i=0; i < inNumFrames; i++)
 	{
-		// store the input values into temp variables for shorter math below
-		in1 = input1[samplecount];
-		in2 = input2[samplecount];
-
 		// do monomerging
-		out1 = (in1 * monomerge_main) + (in2 * monomerge_other);
-		out2 = (in2 * monomerge_main) + (in1 * monomerge_other);
+		const float out1 = (input1[i] * monomerge_main) + (input2[i] * monomerge_other);
+		const float out2 = (input2[i] * monomerge_main) + (input1[i] * monomerge_other);
 
 		// do panning into the output stream
 	#ifdef TARGET_API_VST
 		if (replacing)
 		{
 	#endif
-			outputs[0][samplecount] = (out1 * pan_left1) + (out2 * pan_left2);
-			outputs[1][samplecount] = (out2 * pan_right2) + (out1 * pan_right1);
+			outputs[0][i] = (out1 * pan_left1) + (out2 * pan_left2);
+			outputs[1][i] = (out2 * pan_right2) + (out1 * pan_right1);
 	#ifdef TARGET_API_VST
 		}
 		else
 		{
-			outputs[0][samplecount] += (out1 * pan_left1) + (out2 * pan_left2);
-			outputs[1][samplecount] += (out2 * pan_right2) + (out1 * pan_right1);
+			outputs[0][i] += (out1 * pan_left1) + (out2 * pan_left2);
+			outputs[1][i] += (out2 * pan_right2) + (out1 * pan_right1);
 		}
 	#endif
 	}
