397c37ad9d3041bb8f1d13ef9e29cf0f54e08592
Dr. Tom Murphy VII Ph.D <499244+tom7@users.noreply.github.com>
2003-05-15 23:08:03 +0000

    hey

--- brokenfft/win32/make.bat
+++ brokenfft/win32/make.bat
@@ -1,2 +1,2 @@
 
-cl /nologo /O2 /Ot /Og /Oi /Oy /Gs /GD /I..\vstsdk\ /LD ..\brokenfft\brokenfft.cpp ..\vstsdk\AudioEffect.cpp ..\vstsdk\audioeffectx.cpp ..\fft-lib\fftdom.cpp ..\fft-lib\fftmisc.c ..\fft-lib\fourierf.c -I..\fft-lib\ brokenfft.def /Fec:\progra~1\steinberg\vstplugins\dfx-brokenfft.dll
+cl /nologo /O2 /Ot /Og /Oi /Oy /Gs /I..\vstsdk\ /LD ..\brokenfft\brokenfft.cpp ..\vstsdk\AudioEffect.cpp ..\vstsdk\audioeffectx.cpp ..\fft-lib\fftdom.cpp ..\fft-lib\fftmisc.c ..\fft-lib\fourierf.c -I..\fft-lib\ brokenfft.def /Fec:\progra~1\steinberg\vstplugins\dfx-brokenfft.dll
