3044c6d81d0322fd171ee42115d3c3dfd65259d6
marc3 <marc3@b413c13a-479c-4ba3-9490-20a46969969a>
2003-05-30 01:30:26 +0000

    added comments to the script

--- trunk/scripts/fixpng.sh
+++ trunk/scripts/fixpng.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+# there can be as many input arguments as you want
+# they are all assumed to be PNG file names
+
+# loop through all arguments
 while (( $# >= 1 )); do
+	# create temp output file
+	# output file has all colorspace chunks removed and optimized compression
 	pngcrush -rem gAMA -rem cHRM -rem iCCP -rem sRGB -l 9 "$1" "$1".tmp
+	# remove the original file
 	rm "$1"
+	# replace the original with the new optimized output file
 	mv "$1".tmp "$1"
 	shift
 done
