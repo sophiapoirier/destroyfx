0dfa441032e8dbcfdb2f95464a990e3ce27b73dc
marc3 <2997196+sophiapoirier@users.noreply.github.com>
2003-06-07 22:18:33 +0000

    Scrubby is has an AU DFX GUI

--- scrubby/gui/scrubbyeditor.h
+++ scrubby/gui/scrubbyeditor.h
@@ -1,117 +1,58 @@
-#ifndef __SCRUBBYEDITOR_H
-#define __SCRUBBYEDITOR_H
+#ifndef __SCRUBBY_EDITOR_H
+#define __SCRUBBY_EDITOR_H
+
 
 #include "dfxgui.h"
-#include "dfxguimulticontrols.h"
-#include "multikick.hpp"
-#include "indexbitmap.hpp"
-#include "scrubbykeyboard.hpp"
-
-#include "scrubby.hpp"
+#include "dfxguislider.h"
+#include "dfxguibutton.h"
+#include "dfxguidisplay.h"
 
 
-const CColor kBrownTextCColor = {187, 173, 131, 0};
-
-//-----------------------------------------------------------------------
-class ScrubbyEditor : public AEffGUIEditor, public CControlListener
+//--------------------------------------------------------------------------
+class ScrubbyHelpBox : public DGTextDisplay
 {
 public:
-	ScrubbyEditor(AudioEffect *effect);
-	virtual ~ScrubbyEditor();
+	ScrubbyHelpBox(DfxGuiEditor * inOwnerEditor, DGRect * inRegion, DGImage * inBackground);
+	virtual ~ScrubbyHelpBox();
 
-protected:
-	virtual long getRect(ERect **erect);
-	virtual long open(void *ptr);
-	virtual void close();
+	virtual void draw(CGContextRef inContext, long inPortHeight);
 
-	virtual void setParameter(long index, float value);
-	virtual void valueChanged(CDrawContext* context, CControl* control);
+	virtual void mouseDown(float, float, unsigned long, unsigned long) { }
+	virtual void mouseTrack(float, float, unsigned long, unsigned long) { }
+	virtual void mouseUp(float, float, unsigned long) { }
 
-    virtual void idle();
+	void setDisplayItem(long inItemNum);
 
 private:
-	// controls
-	CHorizontalSliderChunky *octaveMinFader;
-	CHorizontalSliderChunky *octaveMaxFader;
-	CHorizontalRangeSlider *seekRateFader;
-	CHorizontalRangeSlider *seekDurFader;
-	CHorizontalSlider *seekRangeFader;
-	CHorizontalSlider *tempoFader;
-	CHorizontalSlider *predelayFader;
-	//
-	MultiKick *speedModeButton;
-	MultiKick *freezeButton;
-	MultiKick *tempoSyncButton;
-	MultiKick *stereoButton;
-	MultiKick *pitchConstraintButton;
-	COnOffButton *littleTempoSyncButton;
-	//
-	ScrubbyKeyboard *keyboard;
-	CKickButton *transposeDownButton;
-	CKickButton *transposeUpButton;
-	CKickButton *majorChordButton;
-	CKickButton *minorChordButton;
-	CKickButton *allNotesButton;
-	CKickButton *noneNotesButton;
-	//
-	COnOffButton *midiLearnButton;
-	CKickButton *midiResetButton;
-	IndexBitmap *help;
-	CWebLink *DestroyFXlink;
-	CWebLink *SmartElectronixLink;
-
-	// parameter value display boxes
-	CParamDisplay *octaveMinDisplay;
-	CParamDisplay *octaveMaxDisplay;
-	CParamDisplay *seekRateDisplay;
-	CParamDisplay *seekRateRandMinDisplay;
-	CParamDisplay *seekDurDisplay;
-	CParamDisplay *seekDurRandMinDisplay;
-	CParamDisplay *seekRangeDisplay;
-	CParamDisplay *predelayDisplay;
-	CTextEdit *tempoTextEdit;
-
-	// graphics
-	CBitmap *gBackground;
-	CBitmap *gFaderHandle;
-	CBitmap *gGlowingFaderHandle;
-	CBitmap *gFaderHandleLeft;
-	CBitmap *gGlowingFaderHandleLeft;
-	CBitmap *gFaderHandleRight;
-	CBitmap *gGlowingFaderHandleRight;
-	//
-	CBitmap *gSpeedModeButton;
-	CBitmap *gFreezeButton;
-	CBitmap *gTempoSyncButton;
-	CBitmap *gStereoButton;
-	CBitmap *gPitchConstraintButton;
-	CBitmap *gLittleTempoSyncButton;
-	//
-	CBitmap *gKeyboardOff;
-	CBitmap *gKeyboardOn;
-	CBitmap *gTransposeDownButton;
-	CBitmap *gTransposeUpButton;
-	CBitmap *gMajorChordButton;
-	CBitmap *gMinorChordButton;
-	CBitmap *gAllNotesButton;
-	CBitmap *gNoneNotesButton;
-	//
-	CBitmap *gMidiLearnButton;
-	CBitmap *gMidiResetButton;
-	CBitmap *gHelp;
-	CBitmap *gDestroyFXlink;
-	CBitmap *gSmartElectronixLink;
-
-	bool isOpen;
-
-	char *tempoString;
-	static void seekRateDisplayConvert(float value, char *string, void *temporatestring);
-	static float theTempoSync;	// a static version of fTempoSync
-	char *tempoRateString, *tempoRateRandMinString;
-
-	ScrubbyChunk *chunk;
-	bool seekRateClickBetween, seekDurClickBetween;
-	CHorizontalSlider **faders;
+	long itemNum;
 };
+
+
+//-----------------------------------------------------------------------
+class ScrubbyEditor : public DfxGuiEditor
+{
+public:
+	ScrubbyEditor(AudioUnitCarbonView inInstance);
+	virtual ~ScrubbyEditor();
+
+	virtual long open();
+	virtual void mouseovercontrolchanged(DGControl * currentControlUnderMouse);
+
+	void HandleNotesButton(long inNotesButtonType);
+
+private:
+	AUParameterListenerRef parameterListener;
+	AudioUnitParameter tempoSyncAUP;
+	DGSlider * seekRateSlider;
+	DGSlider * seekRateRandMinSlider;
+	DGTextDisplay * seekRateDisplay;
+	DGTextDisplay * seekRateRandMinDisplay;
+
+	ScrubbyHelpBox * helpbox;
+	DGButton ** notesButtons;
+	DGButton * midiLearnButton, * midiResetButton;
+	DGControl * titleArea;
+};
+
 
 #endif
