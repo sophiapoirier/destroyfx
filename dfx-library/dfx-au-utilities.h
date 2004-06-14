d96bf8bf2ae5ad6d66cf630d16335578a4a3ecb1
Sophia Poirier <2997196+sophiapoirier@users.noreply.github.com>
2004-06-14 17:22:31 +0000

    changed to use modified BSD License

--- dfx-library/dfx-au-utilities.h
+++ dfx-library/dfx-au-utilities.h
@@ -1,24 +1,40 @@
 /*
 	DFX AU Utilities is a collection of helpful utility functions for 
 	creating and hosting Audio Unit plugins.
-	Copyright (C) 2003  Marc Genung Poirier
-
-	This library is free software; you can redistribute it and/or 
-	modify it under the terms of the GNU Lesser General Public 
-	License as published by the Free Software Foundation; either 
-	version 2.1 of the License, or (at your option) any later version.
-
-	This library is distributed in the hope that it will be useful, 
-	but WITHOUT ANY WARRANTY; without even the implied warranty of 
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
-	Lesser General Public License for more details.
-
-	You should have received a copy of the GNU Lesser General Public 
-	License along with this library; if not, write to the Free Software 
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-	To contact the author, visit http://destroyfx.org/ and use the contact form.
-	Please refer to the file lgpl.txt for the complete license terms.
+	Copyright (C) 2003-2004  Marc Genung Poirier
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without 
+	modification, are permitted provided that the following conditions 
+	are met:
+	
+	*	Redistributions of source code must retain the above 
+		copyright notice, this list of conditions and the 
+		following disclaimer.
+	*	Redistributions in binary form must reproduce the above 
+		copyright notice, this list of conditions and the 
+		following disclaimer in the documentation and/or other 
+		materials provided with the distribution.
+	*	Neither the name of Destroy FX nor the names of its 
+		contributors may be used to endorse or promote products 
+		derived from this software without specific prior 
+		written permission.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+	FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE 
+	COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+	HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+	STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+	OF THE POSSIBILITY OF SUCH DAMAGE.
+	
+	To contact the author, please visit http://destroyfx.org/ 
+	and use the contact form.
 */
 
 
