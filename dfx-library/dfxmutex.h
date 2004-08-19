71a3f81b4356461e6b7d6b7c8ece44b349398ab6
Sophia Poirier <2997196+sophiapoirier@users.noreply.github.com>
2004-08-19 23:53:29 +0000

    removed all code from the header file and created a separate implementation file dfxmutex.cpp

--- dfx-library/dfxmutex.h
+++ dfx-library/dfxmutex.h
@@ -3,107 +3,44 @@ Destroy FX is a sovereign entity comprised of Marc Poirier & Tom Murphy 7.
 This is our mutex shit.
 ------------------------------------------------------------------------*/
 
-#ifndef __DFXMUTEX_H
-#define __DFXMUTEX_H
-
+#ifndef __DFX_MUTEX_H
+#define __DFX_MUTEX_H
 
 
 #if WIN32
+	// Win32 API
+	#include <Windows.h>
+#elif MAC && !defined(__MACH__)
+	// Multiprocessing Services
+	#include <Multiprocessing.h>
+#else
+	// POSIX threads library
+	#include <pthread.h>
+#endif
 
-struct dfxmutex {
+
+
+class DfxMutex
+{
+public:
+	DfxMutex();
+	~DfxMutex();
+	int grab();
+	int try_grab();
+	int release();
+
+private:
+
+#if WIN32
 	CRITICAL_SECTION c;
-	dfxmutex() { InitializeCriticalSection(&c); }
-	~dfxmutex() { DeleteCriticalSection(&c); }
-	void grab() { EnterCriticalSection(&c); }
-	// map the TryEnterCriticalSection to the sort of error that every other mutex API uses
-	int try_grab() { return (TryEnterCriticalSection(&cs) == 0) ? 3 : 0; }
-	void release() { LeaveCriticalSection(&c); }
-};
-
-
-
-#elif MAC
-
-#ifdef __MACH__
-
-// POSIX
-#include <pthread.h>
-struct dfxmutex {
-	int createErr, deleteErr;
-	pthread_mutex_t pmut;
-	dfxmutex() { createErr = pthread_mutex_init(&pmut, NULL); }
-	~dfxmutex() { deleteErr = pthread_mutex_destroy(&pmut); }
-	int grab () { return pthread_mutex_lock(&pmut); }
-	int try_grab () { return pthread_mutex_trylock(&pmut); }
-	int release () { return pthread_mutex_unlock(&pmut); }
-};
-
-#else
-
-// Multiprocessing Services
-#include <Multiprocessing.h>
-#include <MacErrors.h>
-struct dfxmutex {
-	OSStatus createErr, deleteErr;
+#elif MAC && !defined(__MACH__)
 	MPCriticalRegionID mpcr;
-	Duration timeout;	// in ms
-	dfxmutex()
-	{
-		if ( MPLibraryIsLoaded() )
-			createErr = MPCreateCriticalRegion(&mpcr);
-		else
-			createErr = kMPInsufficientResourcesErr;
-	}
-	~dfxmutex()
-	{
-		if (createErr == noErr)
-			deleteErr = MPDeleteCriticalRegion(mpcr);
-	}
-	OSStatus grab()
-	{
-		if (createErr == noErr) 
-			return MPEnterCriticalRegion(mpcr, kDurationForever);
-		else
-			return createErr;
-	}
-	OSStatus try_grab()
-	{
-		if (createErr == noErr) 
-			return MPEnterCriticalRegion(mpcr, kDurationImmediate);
-		else
-			return createErr;
-	}
-	OSStatus release()
-	{
-		if (createErr == noErr) 
-			return MPExitCriticalRegion(mpcr);
-		else
-			return createErr;
-	}
-};
-
-#endif
-// __MACH__
-
-
-
 #else
-
-/* other platforms don't have mutexes implemented, will just have
-   to get "lucky"... */
-
-struct dfxmutex {
-	dfxmutex() {}
-	~dfxmutex() {}
-	void grab () {}
-	void release () {}
-};
-
-
-
+	pthread_mutex_t pmut;
 #endif
-// platforms
 
+	int createErr, deleteErr;
+};
 
 
 #endif
