8a0c4e6c35b91ab7c5e8f54b7c9f65cb116c60c0
Sophia Poirier <2997196+sophiapoirier@users.noreply.github.com>
2005-08-10 20:40:29 +0000

    created the DGGraphicsContext for abstracting platform-specific graphics drawing contexts in a generalized manner (so far only implemented for the CoreGraphics API in Mac OS X)
    corrected all situations where drawing didn't work correctly in
    compositing windows when FLIP_CG_COORDINATES was not #defined
    
    using TARGET_OS_MAC macro rather than MAC (it's automatically defined
    when building for Mac)

--- dfxgui/dfxguimisc.cpp
+++ dfxgui/dfxguimisc.cpp
@@ -2,6 +2,102 @@
 #include "dfxgui.h"
 
 
+//-----------------------------------------------------------------------------
+DGGraphicsContext::DGGraphicsContext(TARGET_PLATFORM_GRAPHICS_CONTEXT inContext)
+:	context(inContext)
+{
+#if TARGET_OS_MAC
+	portHeight = 0;
+#endif
+}
+
+//-----------------------------------------------------------------------------
+void DGGraphicsContext::setAlpha(float inAlpha)
+{
+#if TARGET_OS_MAC
+	if (context != NULL)
+		CGContextSetAlpha(context, inAlpha);
+#endif
+}
+
+//-----------------------------------------------------------------------------
+void DGGraphicsContext::setAntialias(bool inShouldAntialias)
+{
+#if TARGET_OS_MAC
+	if (context != NULL)
+	{
+		CGContextSetShouldAntialias(context, inShouldAntialias);
+		CGContextSetShouldSmoothFonts(context, inShouldAntialias);
+		if (inShouldAntialias)
+			CGContextSetAllowsAntialiasing(context, inShouldAntialias);
+	}
+#endif
+}
+
+//-----------------------------------------------------------------------------
+void DGGraphicsContext::setFillColor(DGColor inColor, float inAlpha)
+{
+#if TARGET_OS_MAC
+	if (context != NULL)
+		CGContextSetRGBFillColor(context, inColor.r, inColor.g, inColor.b, inAlpha);
+#endif
+}
+
+//-----------------------------------------------------------------------------
+void DGGraphicsContext::setLineWidth(float inLineWidth)
+{
+#if TARGET_OS_MAC
+	if (context != NULL)
+		CGContextSetLineWidth(context, inLineWidth);
+#endif
+}
+
+//-----------------------------------------------------------------------------
+void DGGraphicsContext::fillRect(DGRect * inRect)
+{
+	if (inRect == NULL)
+		return;
+
+#if TARGET_OS_MAC
+	if (context != NULL)
+	{
+		CGRect cgRect = inRect->convertToCGRect(portHeight);
+		CGContextFillRect(context, cgRect);
+	}
+#endif
+}
+
+//-----------------------------------------------------------------------------
+void DGGraphicsContext::strokeRect(DGRect * inRect)
+{
+#if TARGET_OS_MAC
+	if (context != NULL)
+	{
+		CGRect cgRect = inRect->convertToCGRect(portHeight);
+		cgRect = CGRectInset(cgRect, 0.5f, 0.5f);	// CoreGraphics lines are positioned between pixels rather than on them
+		CGContextStrokeRect(context, cgRect);
+//		CGContextStrokeRectWithWidth(inContext, cgRect, inLineWidth);
+	}
+#endif
+}
+
+//-----------------------------------------------------------------------------
+void DGGraphicsContext::strokeLine(float inStartX, float inStartY, float inEndX, float inEndY)
+{
+#if TARGET_OS_MAC
+	if (context != NULL)
+	{
+		CGContextBeginPath(context);
+		CGContextMoveToPoint(context, inStartX, inStartY);
+		CGContextAddLineToPoint(context, inEndX, inEndY);
+		CGContextClosePath(context);
+		CGContextStrokePath(context);
+	}
+#endif
+}
+
+
+
 /***********************************************************************
 	DGImage
 	class for loading and containing images
@@ -10,7 +106,7 @@
 //-----------------------------------------------------------------------------
 DGImage::DGImage(const char * inFileName, DfxGuiEditor * inEditor)
 {
-#if MAC
+#if TARGET_OS_MAC
 	cgImage = NULL;
 
 	// no assumptions can be made about how long the reference is valid, 
@@ -43,9 +139,19 @@ DGImage::DGImage(const char * inFileName, DfxGuiEditor * inEditor)
 				}
 				CGDataProviderRelease(provider);
 
+				bool flipImage = false;
+#ifdef FLIP_CG_COORDINATES
+				flipImage = true;
+#else
+				if (inEditor != NULL)
+				{
+					if ( inEditor->IsCompositWindow() )
+						flipImage = true;
+				}
+#endif
 				// create an uncompressed, alpha-premultiplied bitmap image in memory
 				if (cgImage != NULL)
-					cgImage = PreRenderCGImageBuffer(cgImage);
+					cgImage = PreRenderCGImageBuffer(cgImage, flipImage);
 			}
 			CFRelease(imageResourceURL);
 		}
@@ -61,7 +167,7 @@ DGImage::DGImage(const char * inFileName, DfxGuiEditor * inEditor)
 //-----------------------------------------------------------------------------
 DGImage::~DGImage()
 {
-#if MAC
+#if TARGET_OS_MAC
 	if (cgImage != NULL)
 		CGImageRelease(cgImage);
 	cgImage = NULL;
@@ -71,33 +177,49 @@ DGImage::~DGImage()
 //-----------------------------------------------------------------------------
 long DGImage::getWidth()
 {
-#if MAC
+#if TARGET_OS_MAC
 	if (cgImage != NULL)
 		return CGImageGetWidth(cgImage);
-	else
-		return 1;	// XXX sometimes safer than returning 0
 #endif
+
+	return 1;	// XXX sometimes safer than returning 0
 }
 	
 //-----------------------------------------------------------------------------
 long DGImage::getHeight()
 {
-#if MAC
+#if TARGET_OS_MAC
 	if (cgImage != NULL)
 		return CGImageGetHeight(cgImage);
-	else
-		return 1;	// XXX sometimes safer than returning 0
 #endif
+
+	return 1;	// XXX sometimes safer than returning 0
 }
 
 //-----------------------------------------------------------------------------
-void DGImage::draw(DGRect * inRect, CGContextRef inContext, long inPortHeight, long inXoffset, long inYoffset)
+void DGImage::draw(DGRect * inRect, DGGraphicsContext * inContext, long inXoffset, long inYoffset)
 {
 	if ( (cgImage == NULL) && (inRect == NULL) && (inContext == NULL) )
 		return;
 
 	// convert the DGRect to a CGRect for the CG API stuffs
-	CGRect drawRect = inRect->convertToCGRect(inPortHeight);
+	CGRect drawRect = inRect->convertToCGRect( inContext->getPortHeight() );
+
+// XXX figure out how to make this work
+#if 0
+	// XXX this is supposedly much faster for drawing part of an image in Mac OS X 10.4 or above
+	if (CGImageCreateWithImageInRect != NULL)
+	{
+		CGImageRef subImage = CGImageCreateWithImageInRect(cgImage, drawRect);
+		if (subImage != NULL)
+		{
+			drawRect.origin.x = drawRect.origin.y = 0.0f;
+			CGContextDrawImage(inContext->getPlatformGraphicsContext(), drawRect, subImage);
+			CFRelease(subImage);
+			return;
+		}
+	}
+#endif
 
 	// avoid CoreGraphics' automatic scaled-image-to-fit-drawing-rectangle feature 
 	// by specifying the drawing rect size as the size of the image, 
@@ -108,18 +230,19 @@ void DGImage::draw(DGRect * inRect, CGContextRef inContext, long inPortHeight, l
 #ifndef FLIP_CG_COORDINATES
 	// this positions the image at the top of the region 
 	// rather than at the bottom (upside-down CG coordinates)
-	drawRect.origin.y -= (float) (getHeight() - inRect->h);
+	if (! inContext->isCompositWindow() )
+		drawRect.origin.y -= (float) (getHeight() - inRect->h);
 #endif
 	// take offsets into account
 	drawRect.origin.x -= (float) inXoffset;
-#ifdef FLIP_CG_COORDINATES
-	drawRect.origin.y -= (float) inYoffset;
-#else
-	drawRect.origin.y += (float) inYoffset;	// we have to add because the Y axis is upside-down in CG
+#ifndef FLIP_CG_COORDINATES
+	if (! inContext->isCompositWindow() )
+		inYoffset *= -1.0f;	// we have to add because the Y axis is upside-down in CG
 #endif
+	drawRect.origin.y -= (float) inYoffset;
 
 	// draw
-	CGContextDrawImage(inContext, drawRect, cgImage);
+	CGContextDrawImage(inContext->getPlatformGraphicsContext(), drawRect, cgImage);
 }
 
 //-----------------------------------------------------------------------------
@@ -131,7 +254,7 @@ void ReleasePreRenderedCGImageBuffer(void * info, const void * data, size_t size
 //-----------------------------------------------------------------------------
 // create an uncompressed, alpha-premultiplied bitmap image in memory
 // so far as I can tell, this makes drawing PNG files with CGContextDrawImage about 25 times faster
-CGImageRef PreRenderCGImageBuffer(CGImageRef inImage)
+CGImageRef PreRenderCGImageBuffer(CGImageRef inImage, bool inFlipImage)
 {
 	if (inImage == NULL)
 		return NULL;
@@ -160,10 +283,11 @@ CGImageRef PreRenderCGImageBuffer(CGImageRef inImage)
 		CGContextRef context = CGBitmapContextCreate(buffer, width, height, bitsPerComponent, bytesPerRow, colorSpace, alphaInfo);
 		if (context != NULL)
 		{
-#ifdef FLIP_CG_COORDINATES
-			CGContextTranslateCTM(context, 0.0f, (float)height);
-			CGContextScaleCTM(context, 1.0f, -1.0f);
-#endif
+			if (inFlipImage)
+			{
+				CGContextTranslateCTM(context, 0.0f, (float)height);
+				CGContextScaleCTM(context, 1.0f, -1.0f);
+			}
 			// draw image into context
 			CGRect drawRect = CGRectMake(0.0f, 0.0f, (float)width, (float)height);
 			CGContextDrawImage(context, drawRect, inImage);
