ffc80f246fdc9ef49905fc64e05515ef12aaf955
marc3 <2997196+sophiapoirier@users.noreply.github.com>
2005-08-30 21:29:38 +0000

    changed to always #define TARGET_PLUGIN_HAS_GUI 1

--- monomaker/monomakerdef.h
+++ monomaker/monomakerdef.h
@@ -5,9 +5,7 @@
 #define TARGET_PLUGIN_USES_MIDI	0
 #define TARGET_PLUGIN_IS_INSTRUMENT	0
 #define TARGET_PLUGIN_USES_DSPCORE	0
-#ifndef TARGET_PLUGIN_HAS_GUI
 #define TARGET_PLUGIN_HAS_GUI	1
-#endif
 
 // only necessary if using a custom GUI
 #define PLUGIN_EDITOR_ENTRY_POINT	"MonomakerEditorEntry"
