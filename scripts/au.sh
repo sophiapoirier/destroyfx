6a0bc2035a6641cec2374d8cdc70889a73a8989e
Sophia Poirier <2997196+sophiapoirier@users.noreply.github.com>
2006-01-17 00:23:08 +0000

    improved so that it is much simpler to use (it only requires 1 or at most 2 arguments now:  the full name of the plugin, and optionally a boolean for whether it is a MIDI-enabled plugin)

--- scripts/au.sh
+++ scripts/au.sh
@@ -1,23 +1,48 @@
 #!/bin/sh
 
 # input arguments:
-#  1 - the all-lowercase, all-one-word name of the plugin
-#  2 - the proper name (captilized, maybe spaces) name of the plugin
-#  3 - the name of the plugin's documentation file
-#  4 - whether or not the plugin uses MIDI   (boolean:  0 or 1)
+#  1 - the proper name (captilized, maybe spaces) name of the plugin
+#  2 - whether or not the plugin uses MIDI   (boolean:  0 or 1)
 # examples:
-#  au.sh bufferoverride Buffer\ Override buffer-override.html 1
-#  au.sh monomaker Monomaker monomaker.html 0
+#  au.sh Buffer\ Override 1
+#  au.sh Monomaker 0
+
+if (( $# < 1 )); then
+	echo 
+	echo "   "`basename $0`" pluginname [0|1(uses MIDI)]"
+	echo 
+	exit 1
+fi
+
+# the first argument should be the full proper name of the plugin
+PLUGINNAME="$1"
+# this is the plugin file name
+PLUGINNAME_FILE="dfx ${PLUGINNAME}.component"
+# this is the name mapped to all lowercase letters
+PLUGINNAME_LC="`echo -n "${PLUGINNAME}" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"`"
+# this is the base of the docs file name 
+# (lowercase with spaces turned to hyphens)
+DOCSNAME=`echo -n "${PLUGINNAME_LC}" | sed "y/ /-/"`
+# this is the base of the output archive file name 
+# (lower case with non-alphanumeric characters removed)
+PLUGINNAME_STRIPPED=`echo -n "${DOCSNAME}" | sed "s/\([^0-9A-Za-z]\)//g"`
+
+MIDIPLUGIN=0
+if (( $# >= 2 ))
+then
+	MIDIPLUGIN=$2
+fi
 
 # running strip -x reduces the executable file size 
 # by removing all local symbols and debugging symbols
-strip -x ~/Library/Audio/Plug-Ins/Components/"dfx ${2}.component"/Contents/MacOS/*
+strip -x ~/Library/Audio/Plug-Ins/Components/"${PLUGINNAME_FILE}"/Contents/MacOS/*
 
 # run the input through makedist.sh
 #  * assumes that makedist.sh is in the same directory as this script
-#  * uses the date to generate part of the output file name
+#  * uses the current date to generate part of the output file name
 #  * ummm, also assumes some other stuff being where it happens 
 #    to be on Marc's hard disk, hmmm, maybe I should improve this...
-`dirname $0`/makedist.sh ~/dfx/_misc/distributions/first\ Audio\ Units/${1} au-`date \`\`+%Y-%m-%d''` ~/dfx/vstplugins/docs/${3} "${2}" ${4} ~/Library/Audio/Plug-Ins/Components/"dfx ${2}.component" ~/dfx/docs/COPYING.rtf
+`dirname $0`/makedist.sh ~/dfx/_misc/distributions/first\ Audio\ Units/$PLUGINNAME_STRIPPED au-`date \`\`+%Y-%m-%d''` ~/dfx/vstplugins/docs/${DOCSNAME}.html "${PLUGINNAME}" $MIDIPLUGIN ~/Library/Audio/Plug-Ins/Components/"${PLUGINNAME_FILE}" ~/dfx/docs/COPYING.rtf
+
 
 exit 0
