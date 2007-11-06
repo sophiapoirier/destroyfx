a081a1dc875ab20881f6136c942b45e1e16de540
Sophia Poirier <2997196+sophiapoirier@users.noreply.github.com>
2007-11-06 20:36:06 +0000

    bumped the version value to 1.1.4

--- rmsbuddy/rmsbuddydef.h
+++ rmsbuddy/rmsbuddydef.h
@@ -1,4 +1,9 @@
-#define RMS_BUDDY_VERSION	0x00010103
+#ifndef __RMSBUDDY_DEF_H
+#define __RMSBUDDY_DEF_H
+
+#define RMS_BUDDY_VERSION	0x00010104
 #define RMS_BUDDY_PLUGIN_ID	'rbud'
 #define RMS_BUDDY_MANUFACTURER_ID	'DFX!'
 #define RMS_BUDDY_BUNDLE_ID	"org.destroyfx.RMSBuddy"
+
+#endif
