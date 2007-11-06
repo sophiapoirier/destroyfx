1f69b0e74cd1bbf159f69abb4c4f784bb089428f
sophia3 <2997196+sophiapoirier@users.noreply.github.com>
2007-11-06 16:57:24 +0000

    cleaned up typing and enum definitions

--- dfx-library/dfxpluginproperties.h
+++ dfx-library/dfxpluginproperties.h
@@ -12,56 +12,63 @@ written by Sophia Poirier, January 2003
 #include <AudioToolbox/AudioUnitUtilities.h>	// for kAUParameterListener_AnyParameter
 
 
+
+//-----------------------------------------------------------------------------
 // property IDs for Audio Unit property stuff
-typedef enum {
-	kDfxPluginProperty_PluginPtr = 64000,	// get a pointer to the DfxPlugin
-	kDfxPluginProperty_ParameterValue,		// get/set parameter values (current, min, max, etc.) using specific variable types
+enum {
+kDfxPluginProperty_startID = 64000,
+	kDfxPluginProperty_ParameterValue = kDfxPluginProperty_startID,	// get/set parameter values (current, min, max, etc.) using specific variable types
 	kDfxPluginProperty_ParameterValueConversion,	// expand or contract a parameter value
-	kDfxPluginProperty_ParameterValueString,	// get/set parameter value strings
-	kDfxPluginProperty_RandomizeParameter,	// randomize a parameter
-	kDfxPluginProperty_MidiLearn,			// get/set the MIDI learn state
-	kDfxPluginProperty_ResetMidiLearn,		// clear MIDI parameter assignments
-	kDfxPluginProperty_MidiLearner			// get/set the current MIDI learner parameter
-} DfxPluginProperties;
+	kDfxPluginProperty_ParameterValueString,		// get/set parameter value strings
+	kDfxPluginProperty_RandomizeParameter,			// randomize a parameter
+	kDfxPluginProperty_MidiLearn,					// get/set the MIDI learn state
+	kDfxPluginProperty_ResetMidiLearn,				// clear MIDI parameter assignments
+	kDfxPluginProperty_MidiLearner,					// get/set the current MIDI learner parameter
+kDfxPluginProperty_endOfList,
+	kDfxPluginProperty_NumProperties = kDfxPluginProperty_endOfList - kDfxPluginProperty_startID
+};
 
 
+//-----------------------------------------------------------------------------
 // for kDfxPluginProperty_ParameterValue
-typedef enum {
-	kDfxParameterValueItem_current, 
+enum {
+	kDfxParameterValueItem_current = 0, 
 	kDfxParameterValueItem_previous, 
 	kDfxParameterValueItem_default, 
 	kDfxParameterValueItem_min, 
 	kDfxParameterValueItem_max
-} DfxParameterValueItem;
+};
+typedef uint32_t	DfxParameterValueItem;
 
 typedef struct {
-	long parameterID;
-	DfxParameterValueItem valueItem;
-	DfxParamValueType valueType;
+	DfxParameterValueItem inValueItem;
+	DfxParamValueType inValueType;
 	DfxParamValue value;
 } DfxParameterValueRequest;
 
 
+//-----------------------------------------------------------------------------
 // for kDfxPluginProperty_ParameterValueConversion
-typedef enum {
-	kDfxParameterValueConversion_expand, 
+enum {
+	kDfxParameterValueConversion_expand = 0, 
 	kDfxParameterValueConversion_contract
-} DfxParameterValueConversionType;
+};
+typedef uint32_t	DfxParameterValueConversionType;
 
 typedef struct {
-	long parameterID;
-	DfxParameterValueConversionType conversionType;
+	DfxParameterValueConversionType inConversionType;
 	double inValue;
 	double outValue;
 } DfxParameterValueConversionRequest;
 
 
+//-----------------------------------------------------------------------------
 // for kDfxPluginProperty_ParameterValueString
 typedef struct {
-	long parameterID;
-	long stringIndex;
+	int64_t inStringIndex;
 	char valueString[DFX_PARAM_MAX_VALUE_STRING_LENGTH];
 } DfxParameterValueStringRequest;
+
 
 
 #endif
