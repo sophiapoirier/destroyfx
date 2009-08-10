f8d0a97fa3c09061d069bf6986046295891020a8
Sophia Poirier <2997196+sophiapoirier@users.noreply.github.com>
2009-08-10 15:02:48 +0000

    removed DfxGuiSliderAxis constants in favor of using DGAxis

--- dfxgui/dfxguislider.h
+++ dfxgui/dfxguislider.h
@@ -1,14 +1,30 @@
+/*------------------------------------------------------------------------
+Destroy FX Library (version 1.0) is a collection of foundation code 
+for creating audio software plug-ins.  
+Copyright (C) 2002-2009  Sophia Poirier
+
+This program is free software:  you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+To contact the author, please visit http://destroyfx.org/ 
+and use the contact form.
+------------------------------------------------------------------------*/
+
 #ifndef __DFXGUI_SLIDER_H
 #define __DFXGUI_SLIDER_H
 
 
-#include "dfxgui.h"
-
-
-typedef enum {
-	kDGSliderAxis_vertical,
-	kDGSliderAxis_horizontal
-} DfxGuiSliderAxis;
+#include "dfxguieditor.h"
 
 
 //-----------------------------------------------------------------------------
@@ -16,8 +32,7 @@ class DGSlider : public DGControl
 {
 public:
 	DGSlider(DfxGuiEditor * inOwnerEditor, long inParamID, DGRect * inRegion, 
-				DfxGuiSliderAxis inOrientation, DGImage * inHandleImage, DGImage * inBackgroundImage);
-	virtual ~DGSlider();
+				DGAxis inOrientation, DGImage * inHandleImage, DGImage * inBackgroundImage = NULL);
 
 	virtual void draw(DGGraphicsContext * inContext);
 	virtual void mouseDown(float inXpos, float inYpos, unsigned long inMouseButtons, DGKeyModifiers inKeyModifiers, bool inIsDoubleClick);
@@ -28,7 +43,7 @@ public:
 		{	mouseOffset = inOffset;	}
 
 protected:
-	DfxGuiSliderAxis	orientation;
+	DGAxis		orientation;
 	DGImage *	handleImage;
 	DGImage *	backgroundImage;
 	long		mouseOffset;	// for mouse tracking with click in the middle of the slider handle
