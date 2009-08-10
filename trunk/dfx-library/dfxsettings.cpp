2ad51d10ca964c26717730c6f83c438046bbee53
sophia3 <sophia3@b413c13a-479c-4ba3-9490-20a46969969a>
2009-08-10 04:11:54 +0000

    added the copyright and license term infos that the lawyers said should be at the start of each source file

--- trunk/dfx-library/dfxsettings.cpp
+++ trunk/dfx-library/dfxsettings.cpp
@@ -1,4 +1,28 @@
-/*--------- by Sophia Poirier  ][  April-July + October 2002 ---------*/
+/*------------------------------------------------------------------------
+Destroy FX Library (version 1.0) is a collection of foundation code 
+for creating audio software plug-ins.  
+Copyright (C) 2002-2009  Sophia Poirier
+
+This program is free software:  you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+To contact the author, please visit http://destroyfx.org/ 
+and use the contact form.
+
+Destroy FX is a sovereign entity comprised of Sophia Poirier and Tom Murphy 7.
+Welcome to our settings persistance mess.
+by Sophia Poirier  ][  April-July + October 2002
+------------------------------------------------------------------------*/
 
 #include "dfxsettings.h"
 #include "dfxplugin.h"
@@ -286,7 +310,7 @@ bool DfxSettings::restore(const void * inData, size_t inBufferSize, bool inIsPre
 		return false;
 	}
 	if ( (newSettingsInfo->version < settingsInfo.lowestLoadableVersion) || 
-			(settingsInfo.version < newSettingsInfo->lowestLoadableVersion) )
+			(settingsInfo.version < newSettingsInfo->lowestLoadableVersion) )	// XXX ummm does this second part make any sense?
 	{
 		free(incomingData_copy);
 		return false;
--- trunk/dfx-library/lfo.cpp
+++ trunk/dfx-library/lfo.cpp
@@ -1,4 +1,28 @@
-/*------------------- by Sophia Poirier  ][  January 2002 ------------------*/
+/*------------------------------------------------------------------------
+Destroy FX Library (version 1.0) is a collection of foundation code 
+for creating audio software plug-ins.  
+Copyright (C) 2002-2009  Sophia Poirier
+
+This program is free software:  you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+To contact the author, please visit http://destroyfx.org/ 
+and use the contact form.
+
+Destroy FX is a sovereign entity comprised of Sophia Poirier and Tom Murphy 7.
+Welcome to our LFO.
+by Sophia Poirier  ][  January 2002
+------------------------------------------------------------------------*/
 
 #include "lfo.h"
 
