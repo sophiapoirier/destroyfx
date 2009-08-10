ad94c8261332a4304e06b54958c4b1ce99c7b2c5
Sophia Poirier <2997196+sophiapoirier@users.noreply.github.com>
2009-08-10 03:51:25 +0000

    changed capitalization of some constant names

--- dfx-library/dfxpluginproperties.h
+++ dfx-library/dfxpluginproperties.h
@@ -1,4 +1,24 @@
 /*------------------------------------------------------------------------
+Destroy FX Library (version 1.0) is a collection of foundation code 
+for creating audio software plug-ins.  
+Copyright (C) 2003-2009  Sophia Poirier
+
+This program is free software:  you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+To contact the author, please visit http://destroyfx.org/ 
+and use the contact form.
+
 Destroy FX is a sovereign entity comprised of Sophia Poirier and Tom Murphy 7.  
 These are our extended Audio Unit property IDs and types.  
 written by Sophia Poirier, January 2003
@@ -16,8 +36,8 @@ written by Sophia Poirier, January 2003
 //-----------------------------------------------------------------------------
 // property IDs for Audio Unit property stuff
 enum {
-kDfxPluginProperty_startID = 64000,
-	kDfxPluginProperty_ParameterValue = kDfxPluginProperty_startID,	// get/set parameter values (current, min, max, etc.) using specific variable types
+kDfxPluginProperty_StartID = 64000,
+	kDfxPluginProperty_ParameterValue = kDfxPluginProperty_StartID,	// get/set parameter values (current, min, max, etc.) using specific variable types
 	kDfxPluginProperty_ParameterValueConversion,	// expand or contract a parameter value
 	kDfxPluginProperty_ParameterValueString,		// get/set parameter value strings
 	kDfxPluginProperty_RandomizeParameter,			// randomize a parameter
@@ -25,8 +45,8 @@ kDfxPluginProperty_startID = 64000,
 	kDfxPluginProperty_ResetMidiLearn,				// clear MIDI parameter assignments
 	kDfxPluginProperty_MidiLearner,					// get/set the current MIDI learner parameter
 	kDfxPluginProperty_ParameterMidiAssignment,		// get/set the MIDI assignment for a parameter
-kDfxPluginProperty_endOfList,
-	kDfxPluginProperty_NumProperties = kDfxPluginProperty_endOfList - kDfxPluginProperty_startID
+kDfxPluginProperty_EndOfList,
+	kDfxPluginProperty_NumProperties = kDfxPluginProperty_EndOfList - kDfxPluginProperty_StartID
 };
 
 
