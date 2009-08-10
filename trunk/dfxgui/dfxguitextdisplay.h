3075d52fa9cfe10f5d50db6bc5c71035ca59093b
sophia3 <sophia3@b413c13a-479c-4ba3-9490-20a46969969a>
2009-08-10 15:06:22 +0000

    removed DfxGuiTextDisplayMouseAxis constants in favor of using DGAxis
    renamed displayTextProcedure to DGValue2TextProcedure
    
    DGTextDisplay::drawCFText() now utilizes DGGraphicsContext::drawCFText()

--- trunk/dfxgui/dfxguitextdisplay.h
+++ trunk/dfxgui/dfxguitextdisplay.h
@@ -1,20 +1,36 @@
-#ifndef __DFXGUI_DISPLAY_H
-#define __DFXGUI_DISPLAY_H
+/*------------------------------------------------------------------------
+Destroy FX Library (version 1.0) is a collection of foundation code 
+for creating audio software plug-ins.  
+Copyright (C) 2002-2009  Sophia Poirier
+
+This program is free software:  you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+To contact the author, please visit http://destroyfx.org/ 
+and use the contact form.
+------------------------------------------------------------------------*/
+
+#ifndef __DFXGUI_TEXT_DISPLAY_H
+#define __DFXGUI_TEXT_DISPLAY_H
 
 
-#include "dfxgui.h"
+#include "dfxguieditor.h"
 
-
-typedef enum {
-	kDGTextDisplayMouseAxis_horizontal = 1,
-	kDGTextDisplayMouseAxis_vertical = 1 << 1,
-	kDGTextDisplayMouseAxis_omni = (kDGTextDisplayMouseAxis_horizontal | kDGTextDisplayMouseAxis_vertical)
-} DfxGuiTextDisplayMouseAxis;
 
 const size_t kDGTextDisplay_stringSize = 256;
 
 
-typedef void (*displayTextProcedure) (float inValue, char * outText, void * inUserData);
+typedef void (*DGValue2TextProcedure) (float inValue, char * outText, void * inUserData);
 
 
 #pragma mark -
@@ -23,7 +39,7 @@ class DGTextDisplay : public DGControl
 {
 public:
 	DGTextDisplay(DfxGuiEditor * inOwnerEditor, long inParamID, DGRect * inRegion, 
-					displayTextProcedure inTextProc, void * inUserData, DGImage * inBackground, 
+					DGValue2TextProcedure inTextProc, void * inUserData, DGImage * inBackground, 
 					DGTextAlignment inTextAlignment = kDGTextAlign_left, float inFontSize = 12.0f, 
 					DGColor inFontColor = kDGColor_black, const char * inFontName = NULL);
 	virtual ~DGTextDisplay();
@@ -31,7 +47,7 @@ public:
 	virtual void draw(DGGraphicsContext * inContext);
 	void drawText(DGRect * inRegion, const char * inText, DGGraphicsContext * inContext);
 #if TARGET_OS_MAC
-	OSStatus drawCFText(DGRect * inRegion, const CFStringRef inText, DGGraphicsContext * inContext);
+	OSStatus drawCFText(DGRect * inRegion, CFStringRef inText, DGGraphicsContext * inContext);
 #endif
 
 	virtual void mouseDown(float inXpos, float inYpos, unsigned long inMouseButtons, DGKeyModifiers inKeyModifiers, bool inIsDoubleClick);
@@ -47,12 +63,12 @@ public:
 		{	fontColor = newColor;	}
 	void setAntiAliasing(bool inAntiAlias)
 		{	shouldAntiAlias = inAntiAlias;	}
-	void setMouseAxis(DfxGuiTextDisplayMouseAxis inMouseAxis)
+	void setMouseAxis(DGAxis inMouseAxis)
 		{	mouseAxis = inMouseAxis;	}
 
 protected:
 	DGImage *				backgroundImage;
-	displayTextProcedure	textProc;
+	DGValue2TextProcedure	textProc;
 	void *					textProcUserData;
 
 	DGTextAlignment			alignment;
@@ -61,7 +77,7 @@ protected:
 	char *					fontName;
 	bool					shouldAntiAlias;
 
-	DfxGuiTextDisplayMouseAxis	mouseAxis;	// flags indicating which directions you can mouse to adjust the control value
+	DGAxis					mouseAxis;	// flags indicating which directions you can mouse to adjust the control value
 	float					lastX, lastY;
 };
 
