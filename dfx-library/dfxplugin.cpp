aa4b419cd388ccf0a10147ef465b7e1e58abd7d9
Sophia Poirier <2997196+sophiapoirier@users.noreply.github.com>
2009-08-23 00:52:11 +0000

    added missing Carbon/Carbon.h #include needed for building Mac VST plugins without GUIs

--- dfx-library/dfxplugin.cpp
+++ dfx-library/dfxplugin.cpp
@@ -1,27 +1,27 @@
 /*------------------------------------------------------------------------
-Destroy FX Library (version 1.0) is a collection of foundation code 
-for creating audio software plug-ins.  
+Destroy FX Library is a collection of foundation code 
+for creating audio processing plug-ins.  
 Copyright (C) 2002-2009  Sophia Poirier
 
-This program is free software:  you can redistribute it and/or modify 
+This file is part of the Destroy FX Library (version 1.0).
+
+Destroy FX Library is free software:  you can redistribute it and/or modify 
 it under the terms of the GNU General Public License as published by 
 the Free Software Foundation, either version 3 of the License, or 
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful, 
+Destroy FX Library is distributed in the hope that it will be useful, 
 but WITHOUT ANY WARRANTY; without even the implied warranty of 
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License 
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with Destroy FX Library.  If not, see <http://www.gnu.org/licenses/>.
 
-To contact the author, please visit http://destroyfx.org/ 
-and use the contact form.
+To contact the author, use the contact form at http://destroyfx.org/
 
 Destroy FX is a sovereign entity comprised of Sophia Poirier and Tom Murphy 7.  
 This is our class for E-Z plugin-making and E-Z multiple-API support.
-written by Sophia Poirier, October 2002 - 2004
 ------------------------------------------------------------------------*/
 
 #include "dfxplugin.h"
@@ -38,6 +38,10 @@ written by Sophia Poirier, October 2002 - 2004
 	// for AEffGUIEditor so that we can send it parameter changes.
 	#include "vstgui.h"
 	extern AEffEditor * DFXGUI_NewEditorInstance(DfxPlugin * inEffectInstance);
+#endif
+
+#if TARGET_OS_MAC
+	#include <Carbon/Carbon.h>
 #endif
 
 //#define DFX_DEBUG_PRINT_MUSICAL_TIME_INFO
