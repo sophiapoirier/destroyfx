9d9c4657ff07e7b694dbc60ef608c10541998793
sophia3 <sophia3@b413c13a-479c-4ba3-9490-20a46969969a>
2009-10-07 20:52:19 +0000

    moved constants into DfxIIRfilter class as static members

--- trunk/dfx-library/iirfilter.cpp
+++ trunk/dfx-library/iirfilter.cpp
@@ -1,27 +1,27 @@
 /*------------------------------------------------------------------------
-Destroy FX Library (version 1.0) is a collection of foundation code 
-for creating audio software plug-ins.  
+Destroy FX Library is a collection of foundation code 
+for creating audio processing plug-ins.  
 Copyright (C) 2001-2009  Sophia Poirier
 
-This program is free software:  you can redistribute it and/or modify 
+This file is part of the Destroy FX Library (version 1.0).
+
+Destroy FX Library is free software:  you can redistribute it and/or modify 
 it under the terms of the GNU General Public License as published by 
 the Free Software Foundation, either version 3 of the License, or 
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful, 
+Destroy FX Library is distributed in the hope that it will be useful, 
 but WITHOUT ANY WARRANTY; without even the implied warranty of 
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License 
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with Destroy FX Library.  If not, see <http://www.gnu.org/licenses/>.
 
-To contact the author, please visit http://destroyfx.org/ 
-and use the contact form.
+To contact the author, use the contact form at http://destroyfx.org/
 
 Destroy FX is a sovereign entity comprised of Sophia Poirier and Tom Murphy 7.
-Welcome to our IIR filter.
-by Sophia Poirier  ][  December 2001
+Welcome to our Infinite Impulse Response filter.
 ------------------------------------------------------------------------*/
 
 #include "iirfilter.h"
@@ -29,7 +29,8 @@ by Sophia Poirier  ][  December 2001
 #include "dfxmath.h"
 
 
-const double kDfxIIRFilter_DefaultQ_LP_HP = sqrt(2.0) / 2.0;
+const double DfxIIRfilter::kShelfStartLowpass = 0.333;
+const double DfxIIRfilter::kDefaultQ_LP_HP = sqrt(2.0) / 2.0;
 
 
 //------------------------------------------------------------------------
