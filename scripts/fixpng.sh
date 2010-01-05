ae69855ec078fea84a6f4df128a98e91ea5fc6ac
sophia3 <2997196+sophiapoirier@users.noreply.github.com>
2010-01-05 22:36:33 +0000

    fixed to only replace the original file if pngcrush exited successfully

--- scripts/fixpng.sh
+++ scripts/fixpng.sh
@@ -8,11 +8,11 @@ while (( $# >= 1 )); do
 	# create temp output file
 	# output file has all colorspace chunks removed and optimized compression
 	pngcrush -rem gAMA -rem cHRM -rem iCCP -rem sRGB -l 9 "$1" "$1".tmp
-	# remove the original file
-	rm "$1"
 	# replace the original with the new optimized output file
-	mv "$1".tmp "$1"
+	if (( $? == 0 )); then
+		mv -f "$1".tmp "$1"
+	fi
 	shift
 done
 
-exit 0
+exit $?
