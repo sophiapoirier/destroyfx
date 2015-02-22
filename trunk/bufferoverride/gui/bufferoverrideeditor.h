46ae75d5e263653de82fe6be7db24dd60bb0d5d5
sophia3 <sophia3@b413c13a-479c-4ba3-9490-20a46969969a>
2015-02-22 21:14:39 +0000

    update Buffer Override for 64-bit, VSTGUI-base graphics, AudioComponent API, etc. add dedicated button to GUI for "sync to host tempo" (replaces "go!" button)

--- trunk/bufferoverride/gui/bufferoverrideeditor.h
+++ trunk/bufferoverride/gui/bufferoverrideeditor.h
@@ -1,3 +1,24 @@
+/*------------------------------------------------------------------------
+Copyright (C) 2001-2015  Sophia Poirier
+
+This file is part of Buffer Override.
+
+Buffer Override is free software:  you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+Buffer Override is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with Buffer Override.  If not, see <http://www.gnu.org/licenses/>.
+
+To contact the author, use the contact form at http://destroyfx.org/
+------------------------------------------------------------------------*/
+
 #ifndef __BUFFER_OVERRIDE_EDITOR_H
 #define __BUFFER_OVERRIDE_EDITOR_H
 
@@ -9,16 +30,13 @@
 class BufferOverrideEditor : public DfxGuiEditor
 {
 public:
-	BufferOverrideEditor(AudioUnitCarbonView inInstance);
-	virtual ~BufferOverrideEditor();
+	BufferOverrideEditor(DGEditorListenerInstance inInstance);
 
 	virtual long OpenEditor();
+	virtual void parameterChanged(long inParameterID);
 	virtual void mouseovercontrolchanged(DGControl * currentControlUnderMouse);
 
 private:
-	AUParameterListenerRef parameterListener;
-	AudioUnitParameter bufferSizeTempoSyncAUP, divisorLFOtempoSyncAUP, bufferLFOtempoSyncAUP;
-
 	DGSlider * bufferSizeSlider;
 	DGSlider * divisorLFOrateSlider;
 	DGSlider * bufferLFOrateSlider;
@@ -26,8 +44,6 @@ private:
 	DGTextDisplay * divisorLFOrateDisplay;
 	DGTextDisplay * bufferLFOrateDisplay;
 
-	DGButton * midilearnButton;
-	DGButton * midiresetButton;
 	DGStaticTextDisplay * helpDisplay;
 };
 
