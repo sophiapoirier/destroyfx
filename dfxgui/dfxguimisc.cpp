9afa64171908cbe7229573ae23a7a088630feb67
Dr. Tom Murphy VII Ph.D <499244+tom7@users.noreply.github.com>
2020-07-23 14:08:51 +0000

    This ALSO doesn't work. Gonna give font installing one more shot...

--- dfxgui/dfxguimisc.cpp
+++ dfxgui/dfxguimisc.cpp
@@ -209,6 +209,10 @@ DGColor DGColor::getSystem(System inSystemColorID)
 #pragma mark -
 
 #if TARGET_OS_WIN32
+// TODO: VSTGUI does have support for this, but it uses APIs that are only
+// available in Windows 10 version 1703 (April 2017). Once this version
+// is widely used we can retire this hack, but currently some 35% of users
+// are on Windows 8 or earlier!
 static bool InstallFontResource(const char *resource_name) {
   HRSRC rsrc = FindResourceA(VSTGUI::GetInstance(), resource_name, "TTF");
   if (rsrc == nullptr) {
@@ -229,19 +233,95 @@ static bool InstallFontResource(const char *resource_name) {
     TRACE("LockResource failed??");
     return false;
   }
-    
-  DWORD num_loaded = 0;
-  HANDLE fh = AddFontMemResourceEx(data, rsize, 0, &num_loaded);
-  if (fh == 0) {
-    TRACE("AddFontMemResourceEX failed");
+
+  // It would be nice if we could just do
+  //  AddFontMemResourceEx(data, rsize, 0, &num_loaded);
+  // but, while this successfully installs the font from memory, the
+  // font is not enumerable and so it cannot be found by its name
+  // when VSTGUI tries to create it. Weirdly there is no option to
+  // do this for the memory version, but it is possible with the
+  // version that reads from a file. So, write the resource data to
+  // a temporary file and load it from there.
+  
+  char temp_path[MAX_PATH + 1] = {};
+  if (!GetTempPathA(MAX_PATH, temp_path)) {
+    TRACE("GetTempPathA failed");
     return false;
   }
 
-  if (num_loaded != 1) {
-    TRACE("Didn't get exactly one font, as expected?");
+  // Note that the temp path may not exist, or may not be writable;
+  // it just comes from the environment. So this and the file operations
+  // can definitely fail.
+  char temp_filename[MAX_PATH + 15] = {};
+  if (!GetTempFileNameA(temp_path, "dfx_font_", 0, temp_filename)) {
+    TRACE("GetTempFileName failed");
+    return false;
+  }
+  
+  HANDLE temp_fh = CreateFileA((LPTSTR)temp_filename,
+			       GENERIC_WRITE,
+			       0,
+			       nullptr,
+			       CREATE_ALWAYS,
+			       FILE_ATTRIBUTE_NORMAL,
+			       nullptr);
+  if (temp_fh == INVALID_HANDLE_VALUE) {
+    TRACE("CreateFile failed for temp file");
     return false;
   }
 
+  // Write the data to the temp file.
+  DWORD bytes_written = 0;
+  if (!WriteFile(temp_fh, data, rsize, &bytes_written, nullptr)) {
+    TRACE("WriteFile failed");
+    return false;
+  }
+  if (bytes_written != rsize) {
+    // (Sanity checking; WriteFile should have returned false.)
+    TRACE("Incomplete WriteFile");
+    return false;
+  }
+
+  if (!CloseHandle(temp_fh)) {
+    TRACE("Couldn't close the temp file handle??");
+    return false;
+  }
+
+  if (!AddFontResourceExA(temp_filename,
+			  /* We DO want enumerable, but it can be private
+			     to this process. */
+			  0 /* FR_PRIVATE */,
+			  /* Reserved, must be zero */
+			  0)) {
+    TRACE("AddFontResourceEx failed");
+    return false;
+  }
+
+  // XXX: Couple problems here. If you install with FR_PRIVATE, then VSTGUI
+  // doesn't find the font. :( Perhaps it is "not the same process" somehow?
+  // But if we pass a value of 0, then it works, it's just also available to
+  // other programs for the remainder of the boot. (It seems a bit rude to
+  // have this phantom font installed that the user could use in a document
+  // but then it goes away on the next boot?)
+  // - We can't actually delete the temp file. Looks like you need to pair
+  //   this with RemoveFontResourceExA, and there is somd kind of internal
+  //   reference counting.
+  // - So we could instead move this in to the DFXGUI wrapper, and also
+  //   uninstall/delete the temp file when we unload that. Then we'd only
+  //   have the issue that the user can use the font in another application
+  //   during the period of time they have the plugin GUI open, which is
+  //   not so bad (and actually not uncommon on windows I think).
+  // - Should check to make sure that if the user has the font installed
+  //   normally, we don't end up interfering with it, like uninstalling
+  //   it when we unregister?
+  
+  // Success! Clean up the temp file.
+  if (!DeleteFileA(temp_filename)) {
+    TRACE("Couldn't delete temp file");
+    // (But we did succeed in installing the font...)
+    return false;
+  }
+  
   TRACE("Font successfully installed");
   return true;
 }
