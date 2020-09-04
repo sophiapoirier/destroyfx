9f3c8de9c3d81b61e8debe347ab252b1cdd84a20
sophia3 <sophia3@b413c13a-479c-4ba3-9490-20a46969969a>
2020-09-04 03:38:06 +0000

    update fixpng script to recompress with zopflipng

--- trunk/scripts/fixpng.sh
+++ trunk/scripts/fixpng.sh
@@ -7,10 +7,15 @@
 while (( $# >= 1 )); do
 	# create temp output file
 	# output file has all colorspace chunks removed and optimized compression
-	pngcrush -rem gAMA -rem cHRM -rem iCCP -rem sRGB -l 9 "$1" "$1".tmp
-	# replace the original with the new optimized output file
+	TEMP_FILE_PATH="${1}".tmp
+	pngcrush -rem gAMA -rem cHRM -rem iCCP -rem sRGB -l 9 "${1}" "${TEMP_FILE_PATH}"
+	# optimize the compression
 	if (( $? == 0 )); then
-		mv -f "$1".tmp "$1"
+		zopflipng -m -y "${TEMP_FILE_PATH}" "${TEMP_FILE_PATH}"
+		# replace the original with the new optimized output file
+		if (( $? == 0 )); then
+			mv -f "${TEMP_FILE_PATH}" "${1}"
+		fi
 	fi
 	shift
 done
