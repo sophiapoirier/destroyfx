6b0a421f97a84a2b20a2665687a4a50bb90b4b3d
Tom 7 <499244+tom7@users.noreply.github.com>
2020-11-16 18:09:13 -0500

    New approach to colors/fencing. Add color boxes for the controls in the mockup locations, but probably want to tweak colors a little.

--- makebg/makebg.cc
+++ makebg/makebg.cc
@@ -6,7 +6,6 @@
 #include <memory>
 #include <utility>
 #include <optional>
-// #include <unordered_set>
 #include <set>
 
 #include "base/logging.h"
@@ -16,6 +15,7 @@
 #include "util.h"
 #include "arcfour.h"
 #include "randutil.h"
+#include "color-util.h"
 
 using namespace std;
 
@@ -63,25 +63,92 @@ struct Blue {
   ImageA noise;
 };
 
+// Adapted from image.h; would make sense as a utility...
+// assumes below alpha is FF, and outputs FF alpha.
+static uint32 Blend32(uint32 below, uint32 atop) {
+  uint32 r = (atop >> 24) & 0xFF;
+  uint32 g = (atop >> 16) & 0xFF;
+  uint32 b = (atop >>  8) & 0xFF;  
+  uint32 a = (atop      ) & 0xFF;  
+
+  uint32 old_r = (below >> 24) & 0xFF;
+  uint32 old_g = (below >> 16) & 0xFF;
+  uint32 old_b = (below >> 8) & 0xFF;
+
+  // so a + oma = 255.
+  uint32 oma = 0xFF - a;
+
+  uint32 rr = (((uint32)r * (uint32)a) + (old_r * oma)) / 0xFF;
+  if (rr > 0xFF) rr = 0xFF;
+
+  uint32 gg = (((uint32)g * (uint32)a) + (old_g * oma)) / 0xFF;
+  if (gg > 0xFF) gg = 0xFF;
+
+  uint32 bb = (((uint32)b * (uint32)a) + (old_b * oma)) / 0xFF;
+  if (bb > 0xFF) bb = 0xFF;
+
+  return (rr << 24) | (gg << 16) | (b << 8) | 0xFF;
+}
+
+static uint32 Grey32(uint32 c) {
+  float r = ((c >> 24) & 0xFF) / (float)0xFF;
+  float g = ((c >> 16) & 0xFF) / (float)0xFF;
+  float b = ((c >>  8) & 0xFF) / (float)0xFF;  
+
+  float ll, aa, bb;
+  ColorUtil::RGBToLAB(r, g, b, &ll, &aa, &bb);
+
+  uint8 v = (ll / 100.0f) * 255.0f;
+  return (v << 24) | (v << 16) | (v << 8) | 0xFF;
+}
 
 enum Dir { UP, DOWN, LEFT, RIGHT, };
-static std::optional<std::pair<int, int>> Move(int x, int y, Dir d) {
+static std::pair<int, int> Move(int x, int y, Dir d) {
   switch (d) {
-  case UP:
-    if (y <= 0) return nullopt;
-    return {{x, y - 1}};
-  case DOWN:
-    if (y >= SQUARESH - 1) return nullopt;
-    return {{x, y + 1}};
-  case LEFT:
-    if (x <= 0) return nullopt;
-    return {{x - 1, y}};
-  case RIGHT:
-    if (x >= SQUARESW - 1) return nullopt;
-    return {{x + 1, y}};
-  default: return nullopt;
+  default:
+  case UP: return {x, y - 1};
+  case DOWN: return {x, y + 1};
+  case LEFT: return {x - 1, y};
+  case RIGHT: return {x + 1, y};
   }
 }
+
+struct ThreeColors {
+  uint32 one = 0xFF0000FF;
+  uint32 two = 0x00FF00FF;
+  uint32 three = 0x0000FFFF;
+  ThreeColors(uint32 one, uint32 two, uint32 three) :
+    one(one), two(two), three(three) {}
+
+  ThreeColors Mix(uint32 other) {
+    return ThreeColors(Blend32(one, other),
+		       Blend32(two, other),
+		       Blend32(three, other));
+  }
+  ThreeColors Grey() {
+    return ThreeColors(Grey32(one),
+		       Grey32(two),
+		       Grey32(three));
+  }
+  
+};
+
+// Colorboxes are rectangles (as x,y,w,h in squares space) with
+// a color assignment for the three colors called ONE, TWO, THREE.
+//
+// A colorbox also results in a fence around the rectangle, where
+// we don't allow any joins across that border.
+struct ColorBox {
+  int x = 0, y = 0, w = 0, h = 0;
+  ThreeColors colors;
+  ColorBox(int x, int y, int w, int h,
+	   uint32 one, uint32 two, uint32 three) :
+    x(x), y(y), w(w), h(h), colors(one, two, three) {}
+  ColorBox(int x, int y, int w, int h,
+	   ThreeColors colors) :
+    x(x), y(y), w(w), h(h), colors(colors) {}
+};
+
 
 int main(int argc, char **argv) {
   Blue blue;
@@ -143,30 +210,9 @@ int main(int argc, char **argv) {
   // Now actually render it as colored pixels.
   ImageRGBA img(WIDTH, HEIGHT);  
   img.Clear32(0x000000ff);
-  
-  auto ToColor = [](int x, int y, uint8 v) -> uint32 {
-      // Border squares are drawn in a different
-      // color scheme.
-      if (x == 0 || y == 0 ||
-	  x == SQUARESW - 1 || y == SQUARESH - 1) {
-	switch (v) {
-	case ONE: return 0x7400d2ff;
-	case TWO: return 0x004d65ff;
-	case THREE: return 0x000eb1ff;
-	default: return 0xFF0000FF;
-	}
-      } else {
-	switch (v) {
-	case ONE: return 0x3f6593ff;
-	case TWO: return 0x03621aff;
-	case THREE: return 0x209289ff;
-	default: return 0xFF0000FF;
-	}
-      }
-    };
-  
+    
   // Pass corner_color = color for a crisp box, but setting
-  // the corners 
+  // the corners to 50% alpha makes a nice roundrect effect.
   auto Box = [&img](int x, int y, int w, int h,
 		    uint32 color, uint32 corner_color) {
       // (This could be in ImageRGBA, covering these subtleties)
@@ -194,8 +240,162 @@ int main(int argc, char **argv) {
       Box(x, y, w, h, color, corner_color);
       img.BlendRect32(x + 1, y + 1, w - 2, h - 2, color);
     };
+  
+  // Colors are assigned according to the first containing
+  // rectangle.
+  std::vector<ColorBox> color_boxes;
 
+  ThreeColors standard{0x3f6593ff, 0x03621aff, 0x209289ff};
+
+  // Horizontal sliders.
+  constexpr int SLIDER_X = 3;
+  // Position of the label.
+  constexpr int SLIDER_Y = 11;
+  constexpr int SLIDER_W = 32;
+  constexpr int SLIDER_STRIDE = 6;
+
+  // Yellow group.
+  ThreeColors slider_yellow = standard.Mix(0x858a003f);
+
+  // Each slider is actually two rectangles (of the same color).
+  // The bottom one is basically occluded by the slider itself, but
+  // we don't want joins going into it.
+  for (int i = 0; i < 3; i++) {
+    int yy = SLIDER_Y + (i * SLIDER_STRIDE);
+    color_boxes.emplace_back(SLIDER_X, yy, SLIDER_W, 2,
+			     slider_yellow);
+    color_boxes.emplace_back(SLIDER_X, yy + 2, SLIDER_W, 2,
+			     slider_yellow);
+  }
+
+  ThreeColors slider_red = standard.Mix(0x8a00003f);
+  for (int i = 3; i < 7; i++) {
+    int yy = SLIDER_Y + (i * SLIDER_STRIDE);
+    color_boxes.emplace_back(SLIDER_X, yy, SLIDER_W, 2,
+			     slider_red);
+    color_boxes.emplace_back(SLIDER_X, yy + 2, SLIDER_W, 2,
+			     slider_red);
+  }
+
+  ThreeColors slider_grey = standard.Grey();
+  for (int i = 7; i < 9; i++) {
+    int yy = SLIDER_Y + (i * SLIDER_STRIDE);
+    color_boxes.emplace_back(SLIDER_X, yy, SLIDER_W, 2,
+			     slider_grey);
+    color_boxes.emplace_back(SLIDER_X, yy + 2, SLIDER_W, 2,
+			     slider_grey);
+  }
+
+  ThreeColors slider_pink = standard.Mix(0x9b009e3f);
+  for (int i = 9; i < 10; i++) {
+    int yy = SLIDER_Y + (i * SLIDER_STRIDE);
+    color_boxes.emplace_back(SLIDER_X, yy, SLIDER_W, 2,
+			     slider_pink);
+    color_boxes.emplace_back(SLIDER_X, yy + 2, SLIDER_W, 2,
+			     slider_pink);
+  }
+
+  // Midi learn / reset.
+  // Background color here is not important since they are opaque.
+  constexpr int MIDIBUTTON_Y = SLIDER_Y + 10 * SLIDER_STRIDE;
+  constexpr int MIDIBUTTON_WIDTH = 11;
+  for (int i = 0; i < 2; i++) {
+    int xx = 3 + ((MIDIBUTTON_WIDTH + 2) * i);
+    color_boxes.emplace_back(xx, MIDIBUTTON_Y, MIDIBUTTON_WIDTH, 2,
+			     slider_pink);
+  }
+
+  // Button backgrounds.
+  static constexpr int BUTTON_WIDTH = 12;
+  static constexpr int BUTTON_TOP_HEIGHT = 2;
+  auto Button = [&color_boxes, &standard](int x, int y, int bh = 2) {
+      // Two parts. These are just for fencing; we use standard
+      // background color.
+      color_boxes.emplace_back(x, y, BUTTON_WIDTH, BUTTON_TOP_HEIGHT,
+			       standard);
+      color_boxes.emplace_back(x, y + BUTTON_TOP_HEIGHT,
+			       BUTTON_WIDTH, bh,
+			       standard);
+    };
+
+  constexpr int BUTTON_X = 37;
+  Button(BUTTON_X, SLIDER_Y);
+  Button(BUTTON_X, SLIDER_Y + SLIDER_STRIDE * 1);
+  Button(BUTTON_X, SLIDER_Y + SLIDER_STRIDE * 2);
+  Button(BUTTON_X, SLIDER_Y + SLIDER_STRIDE * 3);  
+
+  constexpr int BUTTON2_X = BUTTON_X + BUTTON_WIDTH + 1;
+  // Algorithm slider is taller.
+  Button(BUTTON2_X, SLIDER_Y, 5);
+  Button(BUTTON2_X, SLIDER_Y + SLIDER_STRIDE * 2);
+  Button(BUTTON2_X, SLIDER_Y + SLIDER_STRIDE * 3);
+
+  // Mix buttons.
+  constexpr int MIX_BUTTON_Y = 69;
+  Button(BUTTON_X, MIX_BUTTON_Y);
+  Button(BUTTON2_X, MIX_BUTTON_Y);  
+
+  // Vertical sliders
+  constexpr int VSLIDER_X = 39;
+  constexpr int VSLIDER_Y = 35;
+  constexpr int VSLIDER_H = 30;
+  constexpr int VSLIDER_STRIDE = 8;
+  for (int i = 0; i < 3; i++) {
+    ThreeColors slider_green = standard.Mix(0x1e8a323f);
+
+    int xx = VSLIDER_X + i * VSLIDER_STRIDE;
+    color_boxes.emplace_back(xx, VSLIDER_Y, 2, VSLIDER_H,
+			     slider_green);
+    color_boxes.emplace_back(xx + 2, VSLIDER_Y, 2, VSLIDER_H,
+			     slider_green);
+
+    // And labels beneath.
+    color_boxes.emplace_back(xx - 1, VSLIDER_Y + VSLIDER_H + 1,
+			     6, 2,
+			     slider_green);
+  }
+  
+  // Help box.
+  constexpr int HELP_HEIGHT = 9;
+  color_boxes.emplace_back(3, SQUARESH - 3 - HELP_HEIGHT,
+			   SQUARESW - 6, HELP_HEIGHT,
+			   standard.Mix(0x0000007F));
+
+  // most of the background.
+  color_boxes.emplace_back(1, 1, SQUARESW - 2, SQUARESH - 2, standard);  
+  // Border goes last. We don't want this as four rectangles (with the
+  // same color) because we do want corners to be able to join in either
+  // direction.
+  color_boxes.emplace_back(0, 0, SQUARESW, SQUARESH,
+			   0x7400d2ff, 0x004d65ff, 0x000eb1ff);
+
+  // Gets the index of the first matching rectangle. Can use the
+  // indices to compute the fences, or to index into color_boxes to
+  // get the color. Returns -1 if not in any rectangle.
+  auto GetRect = [&color_boxes](int x, int y) {
+      for (int i = 0; i < (int)color_boxes.size(); i++) {
+	const ColorBox &box = color_boxes[i];
+	if (x >= box.x && y >= box.y &&
+	    x < box.x + box.w && y < box.y + box.h)
+	  return i;
+      }
+      return -1;
+    };
+  
   // First, draw background colors.
+
+  auto ToColor =
+    [&color_boxes, &GetRect](int x, int y, uint8 v) -> uint32 {
+      const int r = GetRect(x, y);
+      if (r == -1) return 0xFFFF00FF;
+      switch (v) {
+      case ONE: return color_boxes[r].colors.one;
+      case TWO: return color_boxes[r].colors.two;
+      case THREE: return color_boxes[r].colors.three;
+      default: return 0xFF00FFFF;
+      }
+    };
+
   
   for (int row = 0; row < SQUARESH; row++) {
     int y = row * SQUARE;
@@ -207,29 +407,16 @@ int main(int argc, char **argv) {
   }
 
   // Now, fill every cell. But allow joining a box/loop across cells
-  // that match. Do this in a random order. But, do the border first
-  // so that we don't need any additional logic to prevent extensions
-  // into the border; it will all be marked done by the time we get
-  // to the general case.
+  // that match. Do this in a random order.
   ArcFour rc("makebg");
   
   vector<std::pair<int, int>> todo;
   {
-    vector<std::pair<int, int>> todo_border, todo_rest;
-    for (int y = 0; y < SQUARESH; y++) {
-      for (int x = 0; x < SQUARESW; x++) {
-	if (x == 0 || y == 0 || x == SQUARESW - 1 || y == SQUARESH - 1) {
-	  todo_border.emplace_back(x, y);
-	} else {
-	  todo_rest.emplace_back(x, y);
-	}
-      }
-    }
+    for (int y = 0; y < SQUARESH; y++)
+      for (int x = 0; x < SQUARESW; x++)
+	  todo.emplace_back(x, y);
 
-    Shuffle(&rc, &todo_border);
-    Shuffle(&rc, &todo_rest);
-    for (auto p : todo_border) todo.push_back(p);
-    for (auto p : todo_rest) todo.push_back(p);
+    Shuffle(&rc, &todo);
   }
 
   constexpr bool REQUIRE_BG_MATCH = true;  
@@ -242,70 +429,42 @@ int main(int argc, char **argv) {
 	fg.GetPixel(x0, y0) == fg.GetPixel(x1, y1) &&
 	fill.GetPixel(x0, y0) == fill.GetPixel(x1, y1);
   };
+  
 
-  // Note: Can't use unordered_set without custom hash :(
-
-  // Mark occluded regions (e.g. behind sliders) as "done" to start,
-  // so that we don't get bars going into them. This also avoids
-  // painting foreground there, which probably makes the PNG slightly
-  // smaller.
-  std::set<std::pair<int, int>> done;
-
-  constexpr int SLIDER_X = 3;
-  constexpr int SLIDER_Y = 13;
-  constexpr int SLIDER_W = 32;
-  constexpr int SLIDER_H = 2;
-  constexpr int SLIDER_STRIDE = 6;
-
-  constexpr int NUM_SLIDERS = 10;
-  for (int s = 0; s < NUM_SLIDERS; s++) {
-    for (int y = 0; y < SLIDER_H; y++) {
-      int yy = SLIDER_Y + (SLIDER_STRIDE * s) + y;
-      for (int x = 0; x < SLIDER_W; x++) {
-	int xx = SLIDER_X + x;
-	done.insert({xx, yy});
-      }
-    }
-  }
-
+  // Note: Can't use unordered_set without custom hash :(  
+  std::set<std::pair<int, int>> done;  
   for (const auto [x, y] : todo) {
     if (done.find({x, y}) == done.end()) {
       // In order to join across cells, the foreground and
       // fill style must match, as well as the background
       // if that option is enabled.
 
+      // The cells must be in the same colorbox, as well.
+
       // We'll find a rectangle (1xN or Nx1) that includes
       // x,y but potentially extended to neighbors. Inclusive.
       // (Right now we only extend one cell, which might be
       // the best-looking choice anyway.)
-      auto Extend = [&bg, &fg, &fill, &rc, &Same, &done, x, y]() ->
+      auto Extend = [&bg, &fg, &fill, &rc, &GetRect,
+		     &Same, &done, x, y]() ->
 	std::tuple<int, int, int, int> {
-	  vector<Dir> dirs;
-	  // Edges get special treatment.
-	  const bool left = x == 0;
-	  const bool right = x == SQUARESW - 1;
-	  const bool top = y == 0;
-	  const bool bottom = y == SQUARESH - 1;
-	  if ((top || bottom) && !left && !right) {
-	    dirs = {LEFT, RIGHT};
-	  } else if ((left || right) && !top && !bottom) {
-	    dirs = {UP, DOWN};
-	  } else {
-	    dirs = {UP, DOWN, LEFT, RIGHT};
-	  }
+	  vector<Dir> dirs = {UP, DOWN, LEFT, RIGHT};
 	  Shuffle(&rc, &dirs);
 
+	  int r = GetRect(x, y);
+	  CHECK(r != -1);
 	  for (Dir dir : dirs) {
-	    auto neighbor = Move(x, y, dir);
-	    if (neighbor.has_value() &&
-		done.find(neighbor.value()) == done.end() &&
-		Same({x, y}, neighbor.value())) {
+	    const auto [nx, ny] = Move(x, y, dir);
+	    // Note: Out-of-bounds rectangles yield -1, so this
+	    // also does the bounds check for us.
+	    int nr = GetRect(nx, ny);
+	    if (r == nr &&
+		done.find({nx, ny}) == done.end() &&
+		Same({x, y}, {nx, ny})) {
 	      // XXX continue extending...?
-	      int x0 = x, y0 = y;
-	      auto [x1, y1] = neighbor.value();
 	      // printf("Extended %d,%d->%d,%d\n", x0, y0, x1, y1);
-	      return {std::min(x0, x1), std::min(y0, y1),
-		      std::max(x0, x1), std::max(y0, y1)};
+	      return {std::min(x, nx), std::min(y, ny),
+		      std::max(x, nx), std::max(y, ny)};
 	    }
 	  }
 	  // Normal to find no extensions.
@@ -327,11 +486,13 @@ int main(int argc, char **argv) {
 
       switch (fill.GetPixel(x0, y0)) {
       case DOT:
-	FilledBox(xx + 1, yy + 1, (SQUARE * w) - 2, (SQUARE * h) - 2, fg_color, fg_lite);
+	FilledBox(xx + 1, yy + 1, (SQUARE * w) - 2, (SQUARE * h) - 2,
+		  fg_color, fg_lite);
 	break;
       default:
       case LOOP:
-	Box(xx + 1, yy + 1, (SQUARE * w) - 2, (SQUARE * h) - 2, fg_color, fg_lite);
+	Box(xx + 1, yy + 1, (SQUARE * w) - 2, (SQUARE * h) - 2,
+	    fg_color, fg_lite);
 	break;
       }
 
