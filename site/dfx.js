93dcab1451ac0ff92e0e849e9bbda7479d7031f6
Tom 7 <499244+tom7@users.noreply.github.com>
2021-01-09 13:34:29 -0500

    Add javascript-based image overlay for full image viewer. Not 100% on whether the history state stuff works in all browsers, but I think so?

--- site/dfx.js
+++ site/dfx.js
@@ -1,6 +1,6 @@
 // JavaScript utilities used on multiple pages
 function makeElement(what, cssclass, elt) {
-  var e = document.createElement(what);
+  let e = document.createElement(what);
   if (cssclass)
     e.setAttribute('class', cssclass);
   if (elt)
@@ -20,9 +20,50 @@ function TABLE(cssclass, elt) { return makeElement('TABLE', cssclass, elt); }
 function TR(cssclass, elt) { return makeElement('TR', cssclass, elt); }
 function TD(cssclass, elt) { return makeElement('TD', cssclass, elt); }
 function TEXT(contents, elt) {
-  var e = document.createTextNode(contents);
+  let e = document.createTextNode(contents);
   if (elt)
     elt.appendChild(e);
   return e;
 }
 
+function FullImage(f) {
+  let olay = DIV('', document.body);
+  olay.style.position = 'fixed';
+  olay.style.backgroundColor = 'rgba(0,0,0,.75)';
+  olay.style.backdropFilter = 'blur(8px)';
+  olay.style.width = '100%';
+  olay.style.height = '100%';
+  olay.style.zIndex = 999;
+  olay.style.top = 0;
+  olay.style.left = 0;
+  olay.style.cursor = 'pointer';
+  
+  let img = IMG('', olay);
+  img.src = f;
+  img.style.position = 'absolute';
+  img.style.margin = 'auto';
+  img.style.top = 0;
+  img.style.right = 0;
+  img.style.bottom = 0;
+  img.style.left = 0;
+  img.style.border = '2px solid #000';
+  img.style.boxShadow = '5px 5px 32px 16px rgba(0,0,0,.25)';
+
+  window.location = '#img';
+  
+  let clear = () => {
+    if (olay)
+      olay.parentNode.removeChild(olay);
+    olay = null;
+  };
+  
+  window.onpopstate = clear;
+  
+  olay.onclick = (evt) => {
+    evt.stopPropagation();
+    history.back();
+    // Redundant, but in case history is not working
+    // like we expect.
+    clear();
+  };
+}
