f1913ed603aa8981879d1eee04ecfb0e1831e2b5
Sophia Poirier <2997196+sophiapoirier@users.noreply.github.com>
2021-01-24 14:01:03 -0800

    disable four-char-constant compiler warning from vstgui.h inclusion (missing bit from 86cc70d)

--- dfxgui/dfxgui-base.h
+++ dfxgui/dfxgui-base.h
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------
 Destroy FX Library is a collection of foundation code 
 for creating audio processing plug-ins.  
-Copyright (C) 2002-2020  Sophia Poirier
+Copyright (C) 2002-2021  Sophia Poirier
 
 This file is part of the Destroy FX Library (version 1.0).
 
@@ -40,6 +40,7 @@ To contact the author, use the contact form at http://destroyfx.org/
 #pragma clang diagnostic push
 	#pragma clang diagnostic ignored "-Wunused-parameter"
 	#pragma clang diagnostic ignored "-Wnon-virtual-dtor"
+	#pragma clang diagnostic ignored "-Wfour-char-constants"
 	#include "vstgui.h"
 #pragma clang diagnostic pop
 
