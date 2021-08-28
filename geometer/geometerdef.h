c2caff1d46b57bc4dfad392306cf0dc816c8c982
Sophia Poirier <2997196+sophiapoirier@users.noreply.github.com>
2021-08-27 18:45:22 -0700

    Geometer: restore stereo processing for VST (fixes #49)

--- geometer/geometerdef.h
+++ geometer/geometerdef.h
@@ -41,7 +41,7 @@ To contact the author, use the contact form at http://destroyfx.org/
 #define PLUGIN_BACKGROUND_IMAGE_FILENAME	"geometer-background.png"
 #define PLUGIN_BUNDLE_IDENTIFIER	DESTROYFX_BUNDLE_ID_PREFIX PLUGIN_NAME_STRING DFX_BUNDLE_ID_SUFFIX
 #define PLUGIN_COPYRIGHT_YEAR_STRING	"2002-2021"
-#define VST_NUM_CHANNELS	1
+#define VST_NUM_CHANNELS	2
 
 // optional
 #define PLUGIN_DESCRIPTION_STRING	"visually oriented waveform geometry"
