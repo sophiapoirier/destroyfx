sophiapoirier/destroyfx

commit e2362da5eba2cedf55a5426c3b32886cfe425405

remove createbuffers clearbuffers releasebuffers methods from DfxPluginCore since it is the DSP core itself that is a DSP resource managed by DfxPlugin, instantiated only when needed, and regenerated when the audio I/O format changes

dfx-library/dfxplugin.cpp
@@ -148,8 +148,7 @@ DfxPlugin::DfxPlugin(
 // setup the constructors of the inherited base classes, for the appropriate API
 #ifdef TARGET_API_AUDIOUNIT
 	#if TARGET_PLUGIN_IS_INSTRUMENT
-//	TARGET_API_BASE_CLASS(inInstance, UInt32 inNumInputs, UInt32 inNumOutputs, UInt32 inNumGroups = 0), 
-	TARGET_API_BASE_CLASS(inInstance, 0, 1), 
+	TARGET_API_BASE_CLASS(inInstance, 0 /*numInputs*/, 1 /*numOutputs*/), 
 	#else
 	TARGET_API_BASE_CLASS(inInstance), 
 	#endif
@@ -253,15 +252,6 @@ void DfxPlugin::do_PostConstructor()
 	mDfxSettings = std::make_unique<DfxSettings>(PLUGIN_ID, this, settings_sizeOfExtendedData());
 #endif
 
-#if TARGET_PLUGIN_USES_DSPCORE && !defined(TARGET_API_AUDIOUNIT)
-	mDSPCores.reserve(getnumoutputs());
-	for (unsigned long ch = 0; ch < getnumoutputs(); ch++)
-	{
-		auto& dspCore = mDSPCores.emplace_back(dspCoreFactory());
-		dspCore->SetChannelNum(ch);
-	}
-#endif
-
 	dfx_PostConstructor();
 
 	DFX_RegisterIdleClient(this);
@@ -332,7 +322,17 @@ long DfxPlugin::do_initialize()
 		mAsymmetricalInputAudioBuffer.assign(getmaxframes(), 0.0f);
 	#endif
 	}
+
+#ifndef TARGET_API_AUDIOUNIT
+	// regenerate the DSP core instances whenever the audio I/O format changes
+	mDSPCores.clear();
+	mDSPCores.reserve(getnumoutputs());
+	for (unsigned long ch = 0; ch < getnumoutputs(); ch++)
+	{
+		mDSPCores.emplace_back(dspCoreFactory(ch));
+	}
 #endif
+#endif  // TARGET_PLUGIN_USES_DSPCORE
 
 	std::for_each(mSmoothedAudioValues.cbegin(), mSmoothedAudioValues.cend(), 
 				  [sr = getsamplerate()](auto& value){ value.first->setSampleRate(sr); });
@@ -363,6 +363,7 @@ void DfxPlugin::do_cleanup()
 	#ifdef TARGET_API_AUDIOUNIT
 	mAsymmetricalInputBufferList.Deallocate();
 	#else
+	mDSPCores = {};
 	mAsymmetricalInputAudioBuffer = {};
 	#endif
 #endif

dfx-library/dfxplugin-vst.cpp
@@ -61,10 +61,24 @@ void DfxPlugin::resume()
 {
 	updatesamplerate();
 
+	bool const inputChannelCountChanged = !mLastInputChannelCount || (*mLastInputChannelCount != getnuminputs());
+	mLastInputChannelCount = getnuminputs();
+	bool const outputChannelCountChanged = !mLastOutputChannelCount || (*mLastOutputChannelCount != getnumoutputs());
+	mLastOutputChannelCount = getnumoutputs();
+	bool const sampleRateChanged = !mLastSampleRate || (*mLastSampleRate != getsamplerate());
+	mLastSampleRate = getsamplerate();
+	bool const maxFrameCountChanged = !mLastMaxFrameCount || (*mLastMaxFrameCount != getmaxframes());
+	mLastMaxFrameCount = getmaxframes();
+
 	// VST doesn't have initialize and cleanup methods like Audio Unit does, 
 	// so we need to call this here
 	if (!mIsInitialized)
 	{
+		do_initialize();
+	}
+	else if (inputChannelCountChanged || outputChannelCountChanged || sampleRateChanged || maxFrameCountChanged)
+	{
+		do_cleanup();
 		do_initialize();
 	}
 	else

dfx-library/dfxplugin.h
@@ -834,7 +834,7 @@ private:
 #ifdef TARGET_API_AUDIOUNIT
 	ausdk::AUBufferList mAsymmetricalInputBufferList;
 #else
-	[[nodiscard]] std::unique_ptr<DfxPluginCore> dspCoreFactory();
+	[[nodiscard]] std::unique_ptr<DfxPluginCore> dspCoreFactory(unsigned long inChannel);
 	std::vector<std::unique_ptr<DfxPluginCore>> mDSPCores;  // we have to manage this ourselves outside of the AU SDK
 	std::vector<float> mAsymmetricalInputAudioBuffer;
 #endif
@@ -862,6 +862,8 @@ private:
 
 #ifdef TARGET_API_VST
 	bool mIsInitialized = false;
+	std::optional<unsigned long> mLastInputChannelCount, mLastOutputChannelCount, mLastMaxFrameCount;
+	std::optional<double> mLastSampleRate;
 	// VST getChunk() requires that the plugin own the buffer; this contains
 	// the chunk data from the last call to getChunk.
 	std::vector<std::byte> mLastChunk;
@@ -1135,8 +1137,10 @@ public:
 	#ifdef TARGET_API_DSPCORE_CLASS
 		TARGET_API_DSPCORE_CLASS(*inDfxPlugin), 
 	#endif
-		mDfxPlugin(inDfxPlugin)
+		mDfxPlugin(inDfxPlugin),
+		mSampleRate(inDfxPlugin->getsamplerate())
 	{
+		assert(mSampleRate > 0.);
 	}
 
 	virtual ~DfxPluginCore()
@@ -1152,8 +1156,6 @@ public:
 	virtual void process(float const* inStream, float* outStream, unsigned long inNumFrames) = 0;
 	void do_reset()
 	{
-		createbuffers();
-		clearbuffers();
 		reset();
 	}
 	virtual void reset() {}
@@ -1162,24 +1164,17 @@ public:
 	// initially miss that snap if you getValue a smoothed value within processparameters
 	virtual void processparameters() {}
 
-	// ***
-	virtual void createbuffers() {}
-	// ***
-	virtual void clearbuffers() {}
-	// ***
-	virtual void releasebuffers() {}
-
 	auto const getplugin() const noexcept
 	{
 		return mDfxPlugin;
 	}
 	double getsamplerate() const noexcept
 	{
-		return mDfxPlugin->getsamplerate();
+		return mSampleRate;
 	}
 	float getsamplerate_f() const
 	{
-		return mDfxPlugin->getsamplerate_f();
+		return static_cast<float>(mSampleRate);
 	}
 //	DfxParam::Value getparameter(long inParameterIndex) const { return mDfxPlugin->getparameter(inParameterIndex); }
 	double getparameter_f(long inParameterIndex) const
@@ -1269,13 +1264,14 @@ public:
 #else
 	// Mimic what AUKernelBase does here. The channel is just the index
 	// in the mDSPCores vector.
-	void SetChannelNum(unsigned long inChan) noexcept { mChannelNumber = inChan; }
+	void SetChannelNum(unsigned long inChannel) noexcept { mChannelNumber = inChannel; }
 	unsigned long GetChannelNum() const noexcept { return mChannelNumber; }
 #endif
 
 
 private:
 	DfxPlugin* const mDfxPlugin;
+	double const mSampleRate;  // fixed for the lifespan of a DSP core
 
 #ifndef TARGET_API_AUDIOUNIT
 	unsigned long mChannelNumber = 0;
@@ -1395,10 +1391,12 @@ private:
 	// call this in the plugin's constructor if it uses DSP cores for processing
 	#if TARGET_PLUGIN_USES_DSPCORE
 		// DFX_CORE_ENTRY is not useful for APIs other than AU, so it is defined as nothing
-		#define DFX_CORE_ENTRY(PluginCoreClass)											\
-			[[nodiscard]] std::unique_ptr<DfxPluginCore> DfxPlugin::dspCoreFactory()	\
-			{																			\
-				return dspCoreFactory<PluginCoreClass>();								\
+		#define DFX_CORE_ENTRY(PluginCoreClass)																\
+			[[nodiscard]] std::unique_ptr<DfxPluginCore> DfxPlugin::dspCoreFactory(unsigned long inChannel)	\
+			{																								\
+				auto core = dspCoreFactory<PluginCoreClass>();												\
+				core->SetChannelNum(inChannel);																\
+				return core;																				\
 			}
 	#endif
 

stub-dfxplugin/dfxplugin-stub.cpp
@@ -23,7 +23,7 @@ To contact the author, use the contact form at http://destroyfx.org/
 This is a template for making a DfxPlugin.
 ------------------------------------------------------------------------*/
 
-// The gratiutous use of preprocessor defines is to designate code that 
+// The gratuitous use of preprocessor defines is to designate code that 
 // you would use depending on which features or APIs you are supporting.  
 // You wouldn't use all of that stuff in a single plugin.  
 // In this example, it's just in order to show you all of the options.
@@ -95,15 +95,6 @@ DfxStub::DfxStub(TARGET_API_BASE_INSTANCE_TYPE inInstance)
 	// create the other built-in presets, if any
 	// (optional, and not a virtual method, so call it whatever you want)
 	initPresets();
-
-
-// API-specific stuff below
-#ifdef TARGET_API_VST
-	#if TARGET_PLUGIN_HAS_GUI
-	editor = new DfxStubEditor(this);
-	#endif
-#endif
-
 }
 
 void DfxStub::dfx_PostConstructor()
@@ -117,12 +108,6 @@ void DfxStub::dfx_PostConstructor()
 	getsettings().setAllowPitchbendEvents(true);
 	getsettings().setAllowNoteEvents(true);
 #endif
-}
-
-//-------------------------------------------------------------------------
-// do final cleanup here
-DfxStub::~DfxStub()
-{
 }
 
 //-------------------------------------------------------------------------
@@ -154,13 +139,11 @@ void DfxStub::cleanup()
 DfxStubDSP::DfxStubDSP(DfxPlugin* inInstance)
 :	DfxPluginCore(inInstance)
 {
-}
+	auto const bufferSize = std::lround(getsamplerate() * kBufferSize_Seconds);
 
-//-------------------------------------------------------------------------
-DfxStubDSP::~DfxStubDSP()
-{
-	// you must call here because ~DfxPluginCore can't do this for us
-	releasebuffers();
+	// if the sampling rate (and therefore the buffer size) has changed, 
+	// then delete and reallocate the buffers according to the sampling rate
+	buffer.assign(bufferSize, 0.0f);
 }
 
 #endif
@@ -181,8 +164,7 @@ void DfxStubDSP::reset()
 	// reset this buffer position tracker to zero
 	bufferpos = 0;
 
-	// you can confidently access and use any of these values here
-	double audio_sampling_rate = getsamplerate();
+	std::fill(buffer.begin(), buffer.end(), 0.0f);
 }
 
 #else
@@ -198,28 +180,13 @@ void DfxStub::reset()
 }
 #endif
 
+#if !TARGET_PLUGIN_USES_DSPCORE
 //-------------------------------------------------------------------------
 // allocate your audio buffers here
 // this will be called when necessary, handled by DfxPlugin
 // return true on success, false on failure
-// note that createbuffers may be called repeatedly (with no 
-// releasebuffers called in between)
 // createbuffers is called whenever the audio stream format changes 
-// (sampling rate or number of channels), so it is expected that 
-// any implementation will check to see whether or not the 
-// buffers are already allocated and whether or not they need to 
-// be destroyed and reallocated in a different size
-#if TARGET_PLUGIN_USES_DSPCORE
-void DfxStubDSP::createbuffers()
-{
-	auto const bufferSize = std::lround(getsamplerate() * kBufferSize_Seconds);
-
-	// if the sampling rate (and therefore the buffer size) has changed, 
-	// then delete and reallocate the buffers according to the sampling rate
-	buffer.assign(bufferSize, 0.0f);
-}
-
-#else
+// (sampling rate or number of channels)
 void DfxStub::createbuffers()
 {
 	auto const bufferSize = std::lround(getsamplerate() * kBufferSize_Seconds);
@@ -238,29 +205,14 @@ void DfxStub::createbuffers()
 //-------------------------------------------------------------------------
 // deallocate your audio buffers here
 // this will be called when necessary, handled by DfxPlugin
-#if TARGET_PLUGIN_USES_DSPCORE
-void DfxStubDSP::releasebuffers()
-{
-	buffer = {};
-}
-
-#else
 void DfxStub::releasebuffers()
 {
 	buffers = {};
 }
-#endif
 
 //-------------------------------------------------------------------------
 // initialize the values in your audio buffers here
 // this will be called when necessary, handled by DfxPlugin
-#if TARGET_PLUGIN_USES_DSPCORE
-void DfxStubDSP::clearbuffers()
-{
-	std::fill(buffer.begin(), buffer.end(), 0.0f);
-}
-
-#else
 void DfxStub::clearbuffers()
 {
 	for (auto& buffer : buffers)
@@ -268,7 +220,7 @@ void DfxStub::clearbuffers()
 		std::fill(buffer.begin(), buffer.end(), 0.0f);
 	}
 }
-#endif
+#endif  // !TARGET_PLUGIN_USES_DSPCORE
 
 
 

stub-dfxplugin/dfxplugin-stub.h
@@ -26,9 +26,9 @@ This is a template for making a DfxPlugin.
 #pragma once
 
 
-#include "dfxplugin.h"
-
 #include <vector>
+
+#include "dfxplugin.h"
 
 
 //----------------------------------------------------------------------------- 
@@ -45,11 +45,10 @@ enum
 
 
 //----------------------------------------------------------------------------- 
-class DfxStub : public DfxPlugin
+class DfxStub final : public DfxPlugin
 {
 public:
-	DfxStub(TARGET_API_BASE_INSTANCE_TYPE inInstance);
-	virtual ~DfxStub();
+	explicit DfxStub(TARGET_API_BASE_INSTANCE_TYPE inInstance);
 
 	void dfx_PostConstructor() override;
 	long initialize() override;
@@ -96,16 +95,12 @@ private:
 
 #if TARGET_PLUGIN_USES_DSPCORE
 //----------------------------------------------------------------------------- 
-class DfxStubDSP : public DfxPluginCore
+class DfxStubDSP final : public DfxPluginCore
 {
 public:
-	DfxStubDSP(DfxPlugin* inInstance);
-	virtual ~DfxStubDSP();
+	explicit DfxStubDSP(DfxPlugin* inInstance);
 
 	void reset() override;
-	void createbuffers() override;
-	void releasebuffers() override;
-	void clearbuffers() override;
 
 	void processparameters() override;
 	void process(float const* inStream, float* outStream, unsigned long inNumFrames, bool replacing = true) override;

transverb/transverb.h
@@ -39,9 +39,6 @@ public:
   void process(float const* inAudio, float* outAudio, unsigned long inNumFrames) override;
   void reset() override;
   void processparameters() override;
-  void createbuffers() override;
-  void clearbuffers() override;
-  void releasebuffers() override;
 
 private:
   static constexpr long kAudioSmoothingDur_samples = 42;
@@ -69,7 +66,7 @@ private:
 
   std::vector<float> buf1;
   std::vector<float> buf2;
-  int MAXBUF = 0;  // the size of the audio buffer (dependent on sampling rate)
+  int const MAXBUF;  // the size of the audio buffer (dependent on sampling rate)
 
   dfx::IIRFilter filter1, filter2;
   bool speed1hasChanged = false, speed2hasChanged = false;

transverb/transverbformalities.cpp
@@ -94,7 +94,11 @@ void Transverb::dfx_PostConstructor() {
 
 TransverbDSP::TransverbDSP(DfxPlugin* inDfxPlugin)
   : DfxPluginCore(inDfxPlugin),
+    MAXBUF(static_cast<int>(getparametermax_f(kBsize) * 0.001 * getsamplerate())),
     firCoefficientsWindow(dfx::FIRFilter::generateKaiserWindow(kNumFIRTaps, 60.0f)) {
+
+  buf1.assign(MAXBUF, 0.0f);
+  buf2.assign(MAXBUF, 0.0f);
 
   firCoefficients1.assign(kNumFIRTaps, 0.0f);
   firCoefficients2.assign(kNumFIRTaps, 0.0f);
@@ -119,27 +123,9 @@ void TransverbDSP::reset() {
 
   filter1.setSampleRate(getsamplerate());
   filter2.setSampleRate(getsamplerate());
-}
-
-void TransverbDSP::createbuffers() {
-
-  MAXBUF = (int) (getparametermax_f(kBsize) * 0.001 * getsamplerate());
-
-  buf1.assign(MAXBUF, 0.0f);
-  buf2.assign(MAXBUF, 0.0f);
-}
-
-void TransverbDSP::clearbuffers() {
 
   std::fill(buf1.begin(), buf1.end(), 0.0f);
   std::fill(buf2.begin(), buf2.end(), 0.0f);
-}
-
-void TransverbDSP::releasebuffers() {
-
-  MAXBUF = 0;
-  buf1 = {};
-  buf2 = {};
 }
 
 

commit fadaf3da87412376efd0a0c89e9a06a2eec9739b

fix VST std::vector::assign compile error introduced in e2362da5

dfx-library/dfxplugin.cpp
@@ -363,7 +363,7 @@ void DfxPlugin::do_cleanup()
 	#ifdef TARGET_API_AUDIOUNIT
 	mAsymmetricalInputBufferList.Deallocate();
 	#else
-	mDSPCores = {};
+	mDSPCores.clear();
 	mAsymmetricalInputAudioBuffer = {};
 	#endif
 #endif