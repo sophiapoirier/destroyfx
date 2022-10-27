sophiapoirier/destroyfx

commit f3fbebaf51672ca70072cda124f1ec22b41fa9e4

ensure all DSP cores receive the same parameter values for a given render cycle

dfx-library/dfxplugin.cpp
@@ -163,6 +163,9 @@ DfxPlugin::DfxPlugin(
 	mParametersChangedAsOfPreProcess(inNumParameters, false),
 	mParametersTouchedAsOfPreProcess(inNumParameters, false),
 	mParametersChangedInProcessHavePosted(inNumParameters)
+#if TARGET_PLUGIN_USES_DSPCORE
+	, mDSPCoreParameterValuesCache(inNumParameters)
+#endif
 {
 	updatesamplerate();  // XXX have it set to something here?
 
@@ -320,7 +323,9 @@ long DfxPlugin::do_initialize()
 	#endif
 	}
 
-#ifndef TARGET_API_AUDIOUNIT
+	#ifndef TARGET_API_AUDIOUNIT
+	cacheDSPCoreParameterValues();  // AU handles this in its Initialize override, since it must occur before the AU SDK base class' implementation
+
 	// regenerate the DSP core instances whenever the audio I/O format changes
 	mDSPCores.clear();
 	mDSPCores.reserve(getnumoutputs());
@@ -328,7 +333,7 @@ long DfxPlugin::do_initialize()
 	{
 		mDSPCores.emplace_back(dspCoreFactory(ch));
 	}
-#endif
+	#endif
 #endif  // TARGET_PLUGIN_USES_DSPCORE
 
 	std::for_each(mSmoothedAudioValues.cbegin(), mSmoothedAudioValues.cend(), 
@@ -380,6 +385,10 @@ void DfxPlugin::do_cleanup()
 // non-virtual function that calls reset() and insures that some stuff happens
 void DfxPlugin::do_reset()
 {
+#if TARGET_PLUGIN_USES_DSPCORE
+	cacheDSPCoreParameterValues();
+#endif
+
 #ifdef TARGET_API_AUDIOUNIT
 	// no need to do this if we're not even in Initialized state 
 	// because this will basically happen when we become initialized
@@ -388,10 +397,7 @@ void DfxPlugin::do_reset()
 	{
 //		return;
 	}
-	#if !TARGET_PLUGIN_IS_INSTRUMENT
-	// resets the kernels, if any
 	TARGET_API_BASE_CLASS::Reset(kAudioUnitScope_Global, AudioUnitElement(0));
-	#endif
 #endif
 
 	mIsFirstRenderSinceReset = true;
@@ -692,19 +698,24 @@ DfxParam::Value DfxPlugin::getparameter(long inParameterIndex) const
 // return a (hopefully) 0 to 1 scalar version of the parameter's current value
 double DfxPlugin::getparameter_scalar(long inParameterIndex) const
 {
+	return getparameter_scalar(inParameterIndex, getparameter_f(inParameterIndex));
+}
+
+//-----------------------------------------------------------------------------
+double DfxPlugin::getparameter_scalar(long inParameterIndex, double inValue) const
+{
 	if (parameterisvalid(inParameterIndex))
 	{
-		auto const& parameter = mParameters[inParameterIndex];
 		switch (getparameterunit(inParameterIndex))
 		{
 			case DfxParam::Unit::Percent:
 			case DfxParam::Unit::DryWetMix:
-				return parameter.get_f() * 0.01;
+				return inValue * 0.01;
 			case DfxParam::Unit::Scalar:
-				return parameter.get_f();
+				return inValue;
 			// XXX should we not just use contractparametervalue() here?
 			default:
-				return parameter.get_f() / parameter.getmax_f();
+				return inValue / mParameters[inParameterIndex].getmax_f();
 		}
 	}
 	return 0.0;
@@ -858,6 +869,7 @@ std::string DfxPlugin::getparametergroupname(size_t inGroupIndex) const
 //-----------------------------------------------------------------------------
 bool DfxPlugin::getparameterchanged(long inParameterIndex) const
 {
+	assert(std::this_thread::get_id() == mAudioRenderThreadID);  // only valid during audio rendering
 	if (parameterisvalid(inParameterIndex))
 	{
 		return mParametersChangedAsOfPreProcess[inParameterIndex];
@@ -868,6 +880,7 @@ bool DfxPlugin::getparameterchanged(long inParameterIndex) const
 //-----------------------------------------------------------------------------
 bool DfxPlugin::getparametertouched(long inParameterIndex) const
 {
+	assert(std::this_thread::get_id() == mAudioRenderThreadID);  // only valid during audio rendering
 	if (parameterisvalid(inParameterIndex))
 	{
 		return mParametersTouchedAsOfPreProcess[inParameterIndex];
@@ -1935,6 +1948,10 @@ void DfxPlugin::preprocessaudio()
 	mMidiState.preprocessEvents();
 #endif
 
+#if TARGET_PLUGIN_USES_DSPCORE
+	cacheDSPCoreParameterValues();
+#endif
+
 	// fetch the latest musical tempo/time/location information from the host
 	processtimeinfo();
 
@@ -1978,6 +1995,35 @@ void DfxPlugin::do_processparameters()
 }
 
 #if TARGET_PLUGIN_USES_DSPCORE
+//-----------------------------------------------------------------------------
+double DfxPlugin::getdspcoreparameter_gen(long inParameterIndex) const
+{
+	if (parameterisvalid(inParameterIndex))
+	{
+		return contractparametervalue(inParameterIndex, mParameters[inParameterIndex].derive_f(mDSPCoreParameterValuesCache[inParameterIndex]));
+	}
+	return 0.;
+}
+
+//-----------------------------------------------------------------------------
+double DfxPlugin::getdspcoreparameter_scalar(long inParameterIndex) const
+{
+	if (parameterisvalid(inParameterIndex))
+	{
+		getparameter_scalar(inParameterIndex, mParameters[inParameterIndex].derive_f(mDSPCoreParameterValuesCache[inParameterIndex]));
+	}
+	return 0.;
+}
+
+//-----------------------------------------------------------------------------
+void DfxPlugin::cacheDSPCoreParameterValues()
+{
+	for (long i = 0; i < getnumparameters(); i++)
+	{
+		mDSPCoreParameterValuesCache[i] = getparameter(i);
+	}
+}
+
 //-----------------------------------------------------------------------------
 DfxPluginCore* DfxPlugin::getplugincore(unsigned long inChannel) const
 {

dfx-library/dfxplugin-audiounit.cpp
@@ -114,18 +114,17 @@ void DfxPlugin::PreDestructor()
 // this is where DSP-specific resources should be allocated
 OSStatus DfxPlugin::Initialize()
 {
-	OSStatus status = noErr;
-
 #if TARGET_PLUGIN_IS_INSTRUMENT
 	// if this AU supports only specific I/O channel count configs, then check whether the current format is allowed
 	AUSDK_Require(ischannelcountsupported(getnuminputs(), getnumoutputs()), kAudioUnitErr_FormatNotSupported);
-#else
-	// call the inherited class' Initialize routine
-	status = TARGET_API_BASE_CLASS::Initialize();
 #endif
-// TARGET_PLUGIN_IS_INSTRUMENT
 
-	// call our initialize routine
+#if TARGET_PLUGIN_USES_DSPCORE
+	cacheDSPCoreParameterValues();
+#endif
+
+	auto status = TARGET_API_BASE_CLASS::Initialize();
+
 	if (status == noErr)
 	{
 		status = do_initialize();
@@ -146,7 +145,7 @@ void DfxPlugin::Cleanup()
 }
 
 //-----------------------------------------------------------------------------
-// this is called when an audio stream is broken somehow 
+// this is called when an audio stream timeline is broken somehow 
 // (playback stop/restart, change of playback position, etc.)
 // any DSP state variables should be reset here 
 // (contents of buffers, position trackers, IIR filter histories, etc.)
@@ -1514,8 +1513,7 @@ OSStatus DfxPlugin::RestoreState(CFPropertyListRef inData)
 	}
 	if (!success)
 	{
-#endif
-// TARGET_PLUGIN_USES_MIDI
+#endif  // TARGET_PLUGIN_USES_MIDI
 
 // XXX should we rethink this and load parameter settings always before mDfxSettings->restore()?
 	// load the parameter settings that were restored 
@@ -1663,8 +1661,7 @@ OSStatus DfxPlugin::Render(AudioUnitRenderActionFlags& ioActionFlags,
 	return noErr;
 }
 
-#else
-// !TARGET_PLUGIN_IS_INSTRUMENT
+#else  // !TARGET_PLUGIN_IS_INSTRUMENT
 
 //-----------------------------------------------------------------------------
 // this is the audio processing routine
@@ -1672,7 +1669,7 @@ OSStatus DfxPlugin::ProcessBufferLists(AudioUnitRenderActionFlags& ioActionFlags
 									   AudioBufferList const& inBuffer, AudioBufferList& outBuffer, 
 									   UInt32 inFramesToProcess)
 {
-	OSStatus result = noErr;
+	OSStatus status = noErr;
 
 	// do any pre-DSP prep
 	preprocessaudio();
@@ -1703,7 +1700,7 @@ OSStatus DfxPlugin::ProcessBufferLists(AudioUnitRenderActionFlags& ioActionFlags
 
 	// if the plugin uses DSP cores, then we just call the 
 	// inherited base class implementation, which handles "Kernels"
-	result = TARGET_API_BASE_CLASS::ProcessBufferLists(ioActionFlags, *inputBufferPtr, outBuffer, inFramesToProcess);
+	status = TARGET_API_BASE_CLASS::ProcessBufferLists(ioActionFlags, *inputBufferPtr, outBuffer, inFramesToProcess);
 
 #else
 	auto const numInputBuffers = inBuffer.mNumberBuffers;
@@ -1721,8 +1718,7 @@ OSStatus DfxPlugin::ProcessBufferLists(AudioUnitRenderActionFlags& ioActionFlags
 
 	// now do the processing
 	processaudio(mInputAudioStreams_au.data(), mOutputAudioStreams_au.data(), inFramesToProcess);
-#endif
-// end of if/else TARGET_PLUGIN_USES_DSPCORE
+#endif  // end of if/else TARGET_PLUGIN_USES_DSPCORE
 
 	// TODO: allow effects to communicate their output silence status, or calculate time-out from tail size and latency?
 	bool const effectHasTail = !SupportsTail() || (gettailsize_samples() > 0) || (getlatency_samples() > 0);
@@ -1734,10 +1730,9 @@ OSStatus DfxPlugin::ProcessBufferLists(AudioUnitRenderActionFlags& ioActionFlags
 	// do any post-DSP stuff
 	postprocessaudio();
 
-	return result;
+	return status;
 }
-#endif
-// TARGET_PLUGIN_IS_INSTRUMENT
+#endif  // TARGET_PLUGIN_IS_INSTRUMENT
 
 
 
@@ -1823,8 +1818,6 @@ OSStatus DfxPlugin::StopNote(MusicDeviceGroupID inGroupID,
 	handlemidi_noteoff(inGroupID, inNoteInstanceID, 0, inOffsetSampleFrame);
 	return noErr;
 }
-#endif
-// TARGET_PLUGIN_IS_INSTRUMENT
+#endif  // TARGET_PLUGIN_IS_INSTRUMENT
 
-#endif
-// TARGET_PLUGIN_USES_MIDI
+#endif  // TARGET_PLUGIN_USES_MIDI

dfx-library/dfxplugin.h
@@ -641,6 +641,23 @@ public:
 	virtual void settings_crisisAlert(DfxSettings::CrisisReasonFlags /*inFlags*/) {}
 #endif
 
+#if TARGET_PLUGIN_USES_DSPCORE
+	double getdspcoreparameter_f(long inParameterIndex) const
+	{
+		return parameterisvalid(inParameterIndex) ? mParameters[inParameterIndex].derive_f(mDSPCoreParameterValuesCache[inParameterIndex]) : 0.;
+	}
+	int64_t getdspcoreparameter_i(long inParameterIndex) const
+	{
+		return parameterisvalid(inParameterIndex) ? mParameters[inParameterIndex].derive_i(mDSPCoreParameterValuesCache[inParameterIndex]) : 0;
+	}
+	bool getdspcoreparameter_b(long inParameterIndex) const
+	{
+		return parameterisvalid(inParameterIndex) ? mParameters[inParameterIndex].derive_b(mDSPCoreParameterValuesCache[inParameterIndex]) : false;
+	}
+	double getdspcoreparameter_gen(long inParameterIndex) const;
+	double getdspcoreparameter_scalar(long inParameterIndex) const;
+#endif
+
 	// handling of AU properties specific to Logic
 #if defined(TARGET_API_AUDIOUNIT) && LOGIC_AU_PROPERTIES_AVAILABLE
 	UInt32 getSupportedLogicNodeOperationMode() const noexcept
@@ -740,6 +757,7 @@ private:
 
 	void setparameter(long inParameterIndex, DfxParam::Value inValue);
 	DfxParam::Value getparameter(long inParameterIndex) const;
+	double getparameter_scalar(long inParameterIndex, double inValue) const;
 	// synchronize the underlying API/preset/etc. parameter value representation to the current value in DfxPlugin
 	void update_parameter(long inParameterIndex);
 
@@ -780,14 +798,22 @@ private:
 #if TARGET_PLUGIN_USES_DSPCORE
 	template <class DSPCoreClass>
 	[[nodiscard]] std::unique_ptr<DSPCoreClass> dspCoreFactory();
-#ifdef TARGET_API_AUDIOUNIT
+
+	// updates the parameter value cache used by DSP cores
+	// this prevents potential later parameter value updates being visible to higher channel number DSP cores
+	// (must call this immediately before any call path that leads to DSP core operations)
+	void cacheDSPCoreParameterValues();
+
+	std::vector<DfxParam::Value> mDSPCoreParameterValuesCache;
+
+	#ifdef TARGET_API_AUDIOUNIT
 	ausdk::AUBufferList mAsymmetricalInputBufferList;
-#else
+	#else
 	[[nodiscard]] std::unique_ptr<DfxPluginCore> dspCoreFactory(unsigned long inChannel);
 	std::vector<std::unique_ptr<DfxPluginCore>> mDSPCores;  // we have to manage this ourselves outside of the AU SDK
 	std::vector<float> mAsymmetricalInputAudioBuffer;
-#endif
-#endif
+	#endif
+#endif  // TARGET_PLUGIN_USES_DSPCORE
 
 #ifdef TARGET_API_AUDIOUNIT
 	bool mAUElementsHaveBeenCreated = false;
@@ -1115,7 +1141,7 @@ public:
 	// initially miss that snap if you getValue a smoothed value within processparameters
 	virtual void processparameters() {}
 
-	auto const getplugin() const noexcept
+	auto getplugin() const noexcept
 	{
 		return mDfxPlugin;
 	}
@@ -1129,43 +1155,43 @@ public:
 	}
 	double getparameter_f(long inParameterIndex) const
 	{
-		return mDfxPlugin->getparameter_f(inParameterIndex);
+		return mDfxPlugin->getdspcoreparameter_f(inParameterIndex);
 	}
 	int64_t getparameter_i(long inParameterIndex) const
 	{
-		return mDfxPlugin->getparameter_i(inParameterIndex);
+		return mDfxPlugin->getdspcoreparameter_i(inParameterIndex);
 	}
 	bool getparameter_b(long inParameterIndex) const
 	{
-		return mDfxPlugin->getparameter_b(inParameterIndex);
+		return mDfxPlugin->getdspcoreparameter_b(inParameterIndex);
 	}
 	double getparameter_scalar(long inParameterIndex) const
 	{
-		return mDfxPlugin->getparameter_scalar(inParameterIndex);
+		return mDfxPlugin->getdspcoreparameter_scalar(inParameterIndex);
 	}
 	double getparameter_gen(long inParameterIndex) const
 	{
-		return mDfxPlugin->getparameter_gen(inParameterIndex);
+		return mDfxPlugin->getdspcoreparameter_gen(inParameterIndex);
 	}
-	auto getparameterifchanged_f(long inParameterIndex) const
+	std::optional<double> getparameterifchanged_f(long inParameterIndex) const
 	{
-		return mDfxPlugin->getparameterifchanged_f(inParameterIndex);
+		return getparameterchanged(inParameterIndex) ? std::make_optional(getparameter_f(inParameterIndex)) : std::nullopt;
 	}
-	auto getparameterifchanged_i(long inParameterIndex) const
+	std::optional<int64_t> getparameterifchanged_i(long inParameterIndex) const
 	{
-		return mDfxPlugin->getparameterifchanged_i(inParameterIndex);
+		return getparameterchanged(inParameterIndex) ? std::make_optional(getparameter_i(inParameterIndex)) : std::nullopt;
 	}
-	auto getparameterifchanged_b(long inParameterIndex) const
+	std::optional<bool> getparameterifchanged_b(long inParameterIndex) const
 	{
-		return mDfxPlugin->getparameterifchanged_b(inParameterIndex);
+		return getparameterchanged(inParameterIndex) ? std::make_optional(getparameter_b(inParameterIndex)) : std::nullopt;
 	}
-	auto getparameterifchanged_scalar(long inParameterIndex) const
+	std::optional<double> getparameterifchanged_gen(long inParameterIndex) const
 	{
-		return mDfxPlugin->getparameterifchanged_scalar(inParameterIndex);
+		return getparameterchanged(inParameterIndex) ? std::make_optional(getparameter_gen(inParameterIndex)) : std::nullopt;
 	}
-	auto getparameterifchanged_gen(long inParameterIndex) const
+	std::optional<double> getparameterifchanged_scalar(long inParameterIndex) const
 	{
-		return mDfxPlugin->getparameterifchanged_gen(inParameterIndex);
+		return getparameterchanged(inParameterIndex) ? std::make_optional(getparameter_scalar(inParameterIndex)) : std::nullopt;
 	}
 	double getparametermin_f(long inParameterIndex) const
 	{
@@ -1186,10 +1212,6 @@ public:
 	bool getparameterchanged(long inParameterIndex) const
 	{
 		return mDfxPlugin->getparameterchanged(inParameterIndex);
-	}
-	bool getparametertouched(long inParameterIndex) const
-	{
-		return mDfxPlugin->getparametertouched(inParameterIndex);
 	}
 	void registerSmoothedAudioValue(dfx::ISmoothedValue* smoothedValue)
 	{

commit 783fbf928dc6ad754d83c540b63e606e71a0670b

fix bug where DfxPlugin::getdspcoreparameter_scalar always returns zero (introduced in f3fbebaf)

dfx-library/dfxplugin.cpp
@@ -2011,7 +2011,7 @@ double DfxPlugin::getdspcoreparameter_scalar(dfx::ParameterID inParameterID) con
 {
 	if (parameterisvalid(inParameterID))
 	{
-		getparameter_scalar(inParameterID, mParameters[inParameterID].derive_f(mDSPCoreParameterValuesCache[inParameterID]));
+		return getparameter_scalar(inParameterID, mParameters[inParameterID].derive_f(mDSPCoreParameterValuesCache[inParameterID]));
 	}
 	return 0.;
 }